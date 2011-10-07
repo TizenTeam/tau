173f21026dc6588d7039aa1a8ba38104411815fa
Koeun Choi <koeun.choi@samsung.com>
2011-10-07 18:01:35 +0900

    beat-winset test page added
    Change-Id: I101e612ed3fc1ca2cad7d0fde244167154829df3

---
+++ demos/nbeat-black/theme.js
@@ -0,0 +1,8 @@
+(function( $, undefined ) {
+//$.mobile.page.prototype.options.backBtnTheme	= null;
+$.mobile.page.prototype.options.headerTheme		= null;
+$.mobile.page.prototype.options.footerTheme		= null;
+//$.mobile.page.prototype.options.contentTheme	= null;
+$.mobile.page.prototype.options.theme = "s";
+$.mobile.listview.prototype.options.theme = "s";
+})(jQuery);
