ef9f58876b89a771653d0611f0baa01d848e1a40
Noha Park <noha.park@samsung.com>
2011-10-12 20:30:48 +0900

    modify popup

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-sbs -eo lessc /home/nohapark/git/pkgs/WEBUI/web-app-fw/src/themes/nbeat-black/jquery.mobile.popupwindow.less /home/nohapark/git/pkgs/WEBUI/web-app-fw/src/themes/nbeat-black/jquery.mobile.popupwindow.css
 sbs -eo make
 google-chrome --allow-file-access-from-files demos/nbeat-black/index.html
 
