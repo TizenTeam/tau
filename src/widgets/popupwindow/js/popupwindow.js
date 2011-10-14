101e6744a5f3a33a17f630dda8e7ccd7769292a9
Noha Park <noha.park@samsung.com>
2011-10-14 18:09:15 +0900

    modify popup

--- src/widgets/popupwindow/js/popupwindow.js
+++ src/widgets/popupwindow/js/popupwindow.js
@@ -125,6 +125,8 @@ $.widget( "todons.popupwindow", $.mobile.widget, {
 		case "center_button_vertical":
 		case "center_checkbox":
 		case "center_liststyle_1btn":
+		case "center_liststyle_2btn":
+		case "center_liststyle_3btn":
 			//commonly add class for all elements... we will use structured css. 
 			popup.find( ":jqmData(role='title')" )
 				 .wrapAll( "<div class='popup-title'></div>" );
