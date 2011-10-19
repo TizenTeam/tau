3f4f8d0e2b7e5c7de15285cdbc8474e6533dff9e
jinhyuk.jun <jinhyuk.jun@samsung.com>
2011-10-19 15:00:47 +0900

    Modify navigation bar

--- src/jqm/js/jquery.mobile.navbar.js
+++ src/jqm/js/jquery.mobile.navbar.js
@@ -50,30 +50,75 @@ $.widget( "mobile.navbar", $.mobile.widget, {
 			footer = this.element.closest( ":jqmData(role='footer')" );
 			footer.removeClass( "ui-toolbar-" + $.mobile.listview.prototype.options.theme  );
 			footer.removeClass( "ui-bar-" + $.mobile.listview.prototype.options.theme  );
-			footer.addClass( "ui-controlbar-" + $.mobile.listview.prototype.options.theme  );	
+			footer.addClass( "ui-controlbar-" + $.mobile.listview.prototype.options.theme  );
+			footer.addClass( "ui-tabbar-" + $.mobile.listview.prototype.options.theme  );	
 		}
 		else if( style === "toolbar" )
 		{
 			footer = this.element.closest( ":jqmData(role='footer')" );
+			footer.removeClass( "ui-tabbar-" + $.mobile.listview.prototype.options.theme  );
 			footer.removeClass( "ui-bar-" + $.mobile.listview.prototype.options.theme  );
 			footer.addClass( "ui-controlbar-" + $.mobile.listview.prototype.options.theme  );	
 			footer.addClass( "ui-toolbar-" + $.mobile.listview.prototype.options.theme  );			
 		}		
-		else if( style === "left" )
+		else  /* Style left or right */
 		{
 			this.element		
-			.addClass( "ui-controlbar-left" )
+			.addClass( "ui-controlbar-" + style )
 			.end();
 		}
-		else if( style === "right" )
-		{
-			this.element		
-			.addClass( "ui-controlbar-right" )
-			.end();
-		}		
-		//SLP --end		
-		
-		
+	
+		var Prevlist_index = 100;
+
+		 $(".ui-navbar").bind({
+			vclick: function() {	
+				var	element_count = $(".ui-page-active").find('.ui-navbar').find('li').length;
+				var Nextlist_index = $(".ui-page-active").find('li .ui-btn-active').parent().index();				
+
+				style = $(".ui-page-active").find(".ui-navbar").jqmData( "style" );
+
+				if(style == "tabbar"){
+					var list_width = $(".ui-page-active").find('.ui-navbar').width()/element_count;
+					
+					$(".ui-page-active").find(".ui-btn-animation")
+						.css("width", list_width )
+						.css("height","123px" )
+						.css("top", "0px")
+						.css("left", Nextlist_index *list_width );
+					$(".ui-btn-active").children().css("z-index", "20");
+	
+
+					$(".ui-btn-ani-startposition").css("-webkit-transform","translateX("+Prevlist_index *list_width+")");
+					$(".ui-page-active").find(".ui-btn-animation").addClass("ui-btn-ani-startposition");
+					
+					var t=setTimeout("",10);
+	
+					$(".ui-btn-ani-endposition").css("-webkit-transform","translateX("+Nextlist_index *list_width+")");
+					$(".ui-page-active").find(".ui-btn-animation").removeClass("ui-btn-ani-startposition");
+					$(".ui-page-active").find(".ui-btn-animation").addClass("ui-btn-ani-endposition");
+				}					
+				else if(style == "left" || style == "right" ){
+					var list_height = $(".ui-page-active").find('.ui-navbar').height()/element_count;
+
+					$(".ui-page-active").find(".ui-btn-animation")
+						.css("width", "92px" )
+						.css("height",list_height )
+						.css("top", Nextlist_index *list_height + $(document).find(".ui-page-active").find(".ui-header").height() )
+						.css(style, "0px");
+					
+					$(".ui-btn-ani-verticalstartposition").css("-webkit-transform","translateY("+Prevlist_index *list_height+")");
+					$(".ui-page-active").find(".ui-btn-animation").addClass("ui-btn-ani-verticalstartposition");
+					
+					var t=setTimeout("",10);
+
+					$(".ui-btn-ani-verticalendposition").css("-webkit-transform","translateY("+Nextlist_index *list_height+")");
+					$(".ui-page-active").find(".ui-btn-animation").removeClass("ui-btn-ani-verticalstartposition");
+					$(".ui-page-active").find(".ui-btn-animation").addClass("ui-btn-ani-verticalendposition");
+				}
+				
+				Prevlist_index = Nextlist_index;	 			
+			}
+		})
 	}
 });
 
@@ -83,6 +128,36 @@ $( document ).bind( "pagecreate create", function( e ){
 });
 
 /* Jinhyuk_Controlbar */
+$(document).bind( "pageshow", function( e, ui ){
+	var linkURL = this.URL.split("#");
+	var linkid = linkURL[1]; 
+	var	element_count = $("#"+linkid).find(":jqmData(role='navbar')").find('li').length;
+
+	var navbar_filter = $("#"+linkid).find(":jqmData(role='navbar')");
+	var style = navbar_filter.jqmData( "style" );
+
+	if(!($("#"+linkid).find(".ui-navbar div").is("ui-btn-animation")) && style != "toolbar"){					
+		$("#"+linkid).find(".ui-navbar").children().append("<div></div>")
+			.find("div").addClass("ui-btn-animation")	
+			.removeClass("ui-btn-ani-verticalendposition")
+			.removeClass("ui-btn-ani-endposition");
+	
+		if(style =="tabbar"){		
+			$("#"+linkid).find(".ui-navbar").find("div")						
+				.css("width", $("#"+linkid).find('.ui-navbar').width()/element_count )
+				.css("height","123px" )
+				.css("left", 0 * $("#"+linkid).find('.ui-navbar').width()/element_count )
+				.css("top", "0px");
+		}
+		else {
+			$("#"+linkid).find(".ui-navbar").find("div")						
+				.css("width", "92px" )
+				.css("height",$("#"+linkid).find('.ui-navbar').height()/element_count )
+				.css("top", $(document).find(".ui-header").height() )
+				.css(style, "0px");
+		}
+	}
+});
 
 $(document).bind( "pagebeforeshow", function( e, ui ){
 	var linkURL = this.URL.split("#");
@@ -90,34 +165,40 @@ $(document).bind( "pagebeforeshow", function( e, ui ){
 	var	element_count = $("#"+linkid).find(":jqmData(role='navbar')").find('li').length;
 
 	var navbar_filter = $("#"+linkid).find(":jqmData(role='navbar')");
-	var direction = navbar_filter.jqmData( "style" );
+	var style = navbar_filter.jqmData( "style" );
 	
-	if(direction == "toolbar" || direction == "tabbar")
+	if(style == "toolbar" || style == "tabbar")
 	{
 		if(!(navbar_filter.find(".ui-btn-inner").children().is(".ui-icon")))
-		{
-			navbar_filter.find(".ui-btn-inner")
-				.css("padding-top","1.8em")
-				.css("padding-bottom","1.8em");
-		}
+			navbar_filter.find(".ui-btn-inner").addClass("ui-navbar-textonly");
 	}
-	else if(direction == "left"){
-		$("#"+linkid).find(".ui-content").css("padding-"+direction, "105px");
-	}
-	else if(direction == "right"){
-		$("#"+linkid).find(".ui-content").css("padding-"+direction, "80px");
-	}
+	else if(style == "left" || style == "right"){
+		var nav_Top = $(document).find(".ui-page-active").find(".ui-header").height();
+		var nav_Height = ($(document).find(".ui-page-active").height() - nav_Top);
+		var list_Height = nav_Height/element_count;		
 
-	var nav_Top = $(document).find(".ui-page-active").find(".ui-header").height();
-	var nav_Height = ($(document).find(".ui-page-active").height() - nav_Top);
-	var list_Height = nav_Height/element_count;
+		/* list overflow prevent */
+		/* Need to change pixel GUI guideline publish */
+		if(style == "left")
+			$("#"+linkid).find(".ui-content").css("padding-"+style, "105px");
+		else
+			$("#"+linkid).find(".ui-content").css("padding-"+style, "80px");		
+		
+		$(document).find(".ui-controlbar-"+style)
+			.css('top', nav_Top)
+			.css('height', nav_Height)
+			.find("ul").css('height', nav_Height);
+		$(".ui-controlbar-"+style).find('li')
+			.css('height',list_Height )
+			.find('.ui-btn').css('height',list_Height );	
+	}
 	
-	$(document).find(".ui-controlbar-"+direction)
-		.css('top', nav_Top)
-		.css('height', nav_Height)
-		.find("ul").css('height', nav_Height);
-	$(".ui-controlbar-"+direction).find('li')
-		.css('height',list_Height )
-		.find('.ui-btn').css('height',list_Height );
+	/* initialize animation class */
+	if($(".ui-navbar").find("div").is(".ui-btn-animation")){
+		$(".ui-btn-animation")
+			.removeClass("ui-btn-ani-verticalendposition")
+			.removeClass("ui-btn-ani-endposition");				
+		$(".ui-navbar").find("div").remove();
+	}		
 });
 })( jQuery );
