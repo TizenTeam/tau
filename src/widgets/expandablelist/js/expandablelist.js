d0253eb255e6ea164ca630cefb2673135b38f190
Youmin Ha <youmin.ha@samsung.com>
2011-10-26 16:32:12 +0900

    expandable-list: Implement algorithm

--- src/widgets/expandablelist/js/expandablelist.js
+++ src/widgets/expandablelist/js/expandablelist.js
@@ -15,15 +15,39 @@ $.widget("todons.expandablelist", $.mobile.widget, {
 			expanded = e.nextAll(":jqmData(expanded='true')")
 				.filter("[data-for='" + e[0].id + "']");
 
-		//expanded = e.children(":jqmData(expanded='true')");
-		
 		expanded.hide();	// Initially hidden
 
+		// For every expandable, bind event
 		e.bind('vclick', function() {
-			expanded.toggle('fast', 'swing');
-			
+			var 
+			_toggle = function(e) {
+				e.toggle('fast', 'swing');
+				},
+			toggleChildren = function(expanded) {
+				if(expanded[0]) {
+					// If current status is visible, this toggle is hiding.
+					if($(expanded[0]).is(":visible")) {
+						var toBeToggled = null;
+						$.each(expanded, function(idx, e) {
+							var elem = $(e);
+							if ('' == elem[0].id) return true; 	// continue
+							var children = elem.nextAll(":jqmData(expanded='true')")
+								.filter("[data-for='" + elem[0].id + "']")
+								.filter(":visible");
+							_toggle(children);
+							toggleChildren(children);
+						});
+					}
+				}
+			}
+			_toggle(expanded);
+			toggleChildren(expanded);
 		});
-	}
+	},
+
+	stepToggle: function (expanded, self) {
+
+	},
 });	// end: $.widget()
  
 
