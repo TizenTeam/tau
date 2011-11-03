64d120b4dfd17ee8830a5ff86571fcaac68b32c4
Youmin Ha <youmin.ha@samsung.com>
2011-11-03 20:02:43 +0900

    use CSS3 transitin for hide/show effect in expandable list

--- src/widgets/expandablelist/js/expandablelist.js
+++ src/widgets/expandablelist/js/expandablelist.js
@@ -10,50 +10,70 @@ $.widget("todons.expandablelist", $.mobile.widget, {
 		initSelector: ":jqmData(expandable='true')",
 	},
 
+	_hide: function(e) {
+		$(e).removeClass('ui-li-expand-transition-show')
+			.addClass('ui-li-expand-transition-hide');
+		},
+	_show: function(e) {
+		$(e).removeClass('ui-li-expand-transition-hide')
+			.addClass('ui-li-expand-transition-show');
+		},
+	_toggle: function(self, e) {
+		if (self._is_hidden(e)) self._show(e);
+		else self._hide(e);
+		},
+	_is_hidden: function(e) {
+		return ( $(e).height() == 0);
+		},
+
+	_toggle_children: function(self, is_hidden, expanded) {
+		if(is_hidden) return;
+		$.each(expanded, function(idx, e) {
+			var elem = $(e);
+			if ('' == elem[0].id) return true;	// continue
+			var children = elem.nextAll(":jqmData(expanded-by='"+elem[0].id +"')").filter(":visible");	// Only 
+			children.each(function(i, e) { 
+				self._toggle(self, e);
+				});
+			self._toggle_children(self, is_hidden, children);
+			});
+		},
+
+
 	_create: function () {
 		var e = this.element,
 			self = this,
-			expanded = e.nextAll(":jqmData(expanded-by='" + e[0].id + "')");
+			expanded = e.nextAll(":jqmData(expanded-by='" + e[0].id + "')"),
+			initial_expansion = e.data("initial-expansion");
 
-		expanded.hide();	// Initially hidden
+		// Set initial status
+		if(initial_expansion == true) {
+			expanded.each(function(i, e) { 
+				self._show(e); 
+				});
+		} else {
+			expanded.each(function(i, e) { 
+				self._hide(e); 
+				});
+		}
+
+		expanded.addClass("ui-li-expanded");
 
 		e.find( ":jqmData(expandable='true')" )
 			 .wrapInner( '<img src="thumbnail.jpg" class="ui-li-expanded-icon">' );
 
-
 		// For every expandable, bind event
 		e.bind('vclick', function() {
-			var
-			_toggle = function(e) {
-				e.toggle('fast', 'swing');
-			},
-			toggleChildren = function(expanded) {
-				if(expanded[0]) {
-					// If current status is visible, this toggle is hiding.
-					if($(expanded[0]).is(":visible")) {
+			var is_hidden = self._is_hidden(expanded[0]);
 
-						var toBeToggled = null;
-						$.each(expanded, function(idx, e) {
-
-							var elem = $(e);
-							if ('' == elem[0].id) return true;	// continue
-							var children = elem.nextAll(":jqmData(expanded-by='"+elem[0].id +"')").filter(":visible");
-
-							_toggle(children);
-							toggleChildren(children);
-
-						});
-					}
-				}
-			}
-			_toggle(expanded);
-			toggleChildren(expanded);
+			expanded.each(function(i, e) { 
+				self._toggle(self, e); 
+				});
+			if(! is_hidden) self._toggle_children(self, is_hidden, expanded);
 		});
 	},
 
-	stepToggle: function (expanded, self) {
 
-	},
 });	// end: $.widget()
 
 
