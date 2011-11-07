26cadd937ab4be534414bc609649aa3abc0fad08
Koeun Choi <koeun.choi@samsung.com>
2011-11-07 15:30:19 +0900

    [radio] test js modified

--- demos/nbeat-black/widgets/radio/radio.js
+++ demos/nbeat-black/widgets/radio/radio.js
@@ -1,6 +1,7 @@
 $( "#radio-demo" ).live("pagecreate", function () {
 	$("input[type='radio']").bind( "change", function(event, ui) {
-		$( ".triggered-radio" ).text( this.id + " is " + this.checked );
+		if( this.checked )
+			$( ".triggered-radio" ).text( this.id + " is selected..." );
 	});
 
 });
