2b36543176777ea9e5640fd1b39ce82a1d264b74
Youmin Ha <youmin.ha@samsung.com>
2011-11-09 20:33:02 +0900

    Adjust pagecontrol images

--- src/widgets/pagecontrol/js/pagecontrol.js
+++ src/widgets/pagecontrol/js/pagecontrol.js
@@ -7,21 +7,64 @@
 
 $.widget("todons.pagecontrol", $.mobile.widget, {
 	options: {
+		initSelector: ":jqmData(role='pagecontrol')",
 	},
 
 	_create: function () {
-		var e = this.element;
-		
-		e.attr("a", "1");
-		alert("create! " + e.attr("a"));
 	},
 
 	_init: function() {
-		e.attr("a", "2");
-		alert("init! " + e.attr("a"));
-	}
+		var e = this.element,
+			maxVal = e.data("max"),
+			currentVal = e.data("current"),
+			i = 0,
+			btn = null,
+			buf = null;
+		
+		// Set default values
+		if(!maxVal) {
+			maxVal = 1;
+		} else if(maxVal > 10) {
+			maxVal = 10;
+		}
+		e.data("max", maxVal);
 
+		if(!currentVal) {
+			currentVal = 1;
+		}
+		e.data("current", currentVal);
 
+		// Set class
+		e.addClass('pagecontrol');
+
+		// Calculate left/right margin
+
+		// Add dot icons
+		for(i=1; i<=maxVal; i++) {
+			btn = $('<div class="page_n page_n_dot"></div>');
+			e.append(btn);
+			if(i == currentVal) {
+				btn.removeClass('page_n_dot').
+					addClass('page_n_'+i);
+			}
+		}
+
+		e.bind('vclick', function() {
+			
+		});
+
+	},
+	
+	setChangeCallback: function( callback ) {
+
+	},
 });	// end: $.widget()
+
+
+$(document).bind("pagecreate create", function(e) {
+		$($.todons.pagecontrol.prototype.options.initSelector, e.target)
+		.not(":jqmData(role='none'), :jqmData(role='nojs')")
+		.pagecontrol();
+});
 
 }) (jQuery);
