03d9be416fa6804ffef00b92d78e13cd8a4547d2
Lee Wongi <wongi11.lee@samsung.com>
2011-11-10 14:41:10 +0900

    Update Phonebook demo : Add search bar.

--- demos/phonebook/config.js
+++ demos/phonebook/config.js
@@ -1,4 +1,7 @@
-S.load('init.js');
+S.load(
+		'js/phonebook.js',
+        'init.js'
+);
 
 /* link custom stylesheet */
 S.css.load('css/phonebook.css');
--- demos/phonebook/init.js
+++ demos/phonebook/init.js
@@ -1,1 +1,14 @@
+$("#queryInput").live( "change", function(events, ui) {
+	
+});
+
+$("#queryInput").live( "input", function(events, ui) {
+	
+	var query = "";
+	query = ".*" + $("#queryInput").val();
+	
+	search(phonebookDB, "name_first", query);
+});
+
+
 
