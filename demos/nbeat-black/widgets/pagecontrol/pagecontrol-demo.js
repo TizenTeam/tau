fe8fbafd3fa13f3e7e2b3464ceceb059f0c63819
Youmin Ha <youmin.ha@samsung.com>
2011-11-11 16:48:42 +0900

    pagecontrol: Add value change example

--- demos/nbeat-black/widgets/pagecontrol/pagecontrol-demo.js
+++ demos/nbeat-black/widgets/pagecontrol/pagecontrol-demo.js
@@ -2,10 +2,13 @@
 $('#pagecontrol').live('pageshow', function() {
 	var i = 1;
 	for(i=1; i<=10; i++) {
+		// Example: Set value change callback
 		$('#p'+i).bind("change", function(event, value) {
 			var log = 'Changed value to ' + value;
 			$("#txt").html(log);
 		});
+		// Example: Set value by random
+		$('#p'+i).trigger('change', Math.floor(Math.random() * i + 1));
 	}
 });
 
