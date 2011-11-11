3beb818f263b1d6c1fe2dca2f44275ae85625b34
Lee Wongi <wongi11.lee@samsung.com>
2011-11-11 10:59:33 +0900

    Add detail view on phonebook. 1st.

--- demos/phonebook/js/phonebook.js
+++ demos/phonebook/js/phonebook.js
@@ -1,18 +1,35 @@
-function search(dbArray, fields, regEx)
-{
-	/* Search keyword string from dbArray's fields, return an array. */
-	var search_result = new Array();
+Phonebook = {
+	search: function(dbArray, fields, regEx)
+	{
+		/* Search keyword string from dbArray's fields, return an array. */
+		var search_result = new Array();
+		
+		var query = dbArray;/* + "." + fields;*/
+		
+		$.each(dbArray, function(i, v) {
+	        if (v.name_first.search(new RegExp(regEx)) != -1) {
+	            search_result.push(v);
+	            return;
+	        }
+	    });
+		
+		$("ul.ui-virtual-list-container").virtuallistview("recreate", search_result);
+		
+		return search_result;
+	},
 	
-	var query = dbArray;/* + "." + fields;*/
-	
-	$.each(dbArray, function(i, v) {
-        if (v.name_first.search(new RegExp(regEx)) != -1) {
-            search_result.push(v);
-            return;
-        }
-    });
-	
-	$("ul.ui-virtual-list-container").virtuallistview("recreate", search_result);
-	
-	return search_result;
-}
+	getDetailview: function(dbArray, pbid)
+	{
+		var selectedContact;
+		
+		$.each(dbArray, function(i, v) {
+	        if (v.Luid == pbid) {
+	        	alert("Find : " + v.Luid + " : " + v.name_first);
+	        	
+	        	selectedContact = v;
+	        	
+	            return;
+	        }
+		});
+	}
+} ;
