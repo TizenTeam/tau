f00a6683c031b3e9be40199ba7fec52a704d100b
Youmin Ha <youmin.ha@samsung.com>
2011-11-15 17:04:05 +0900

    jqm bugfix: When a jqm custom event(tap, swipe) are triggered, clear event.liveFired to prevent not firing that custom event, because basic event firing in liveFired() sets this value to prevent multiple live event firing. This patch could issue some problem!

--- src/jqm/js/jquery.mobile.event.js
+++ src/jqm/js/jquery.mobile.event.js
@@ -105,8 +105,6 @@ $.event.special.tap = {
 			function clickHandler(event) {
 				clearTapHandlers();
 
-				alert('xxx');
-
 				// ONLY trigger a 'tap' event if the start target is
 				// the same as the stop target.
 				if ( origTarget == event.target ) {
