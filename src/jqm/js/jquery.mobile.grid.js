67e443f188325b46d6e90ba06f4f0736088e7e63
jinhyuk.jun <jinhyuk.jun@samsung.com>
2011-11-23 18:04:20 +0900

    move grid to navbar

--- src/jqm/js/jquery.mobile.grid.js
+++ src/jqm/js/jquery.mobile.grid.js
@@ -9,54 +9,44 @@
 
 $.fn.grid = function( options ) {
 	return this.each(function() {
+		var $this = $( this ),
+			o = $.extend({
+				grid: null
+			},options),
+			$kids = $this.children(),
+			gridCols = {solo:1, a:2, b:3, c:4, d:5},
+			grid = o.grid,
+			iterator;
 
-
-		if($(this).parents(":jqmData(role='navbar')").is(":jqmData(style='left')") ||
-		  $(this).parents(":jqmData(role='navbar')").is(":jqmData(style='right')") ){
-			var navPage = $(this).parents( ".ui-content" );
-			navPage.css('padding', '0');
-
-		}
-		else{
-			var $this = $( this ),
-				o = $.extend({
-					grid: null
-				},options),
-				$kids = $this.children(),
-				gridCols = {solo:1, a:2, b:3, c:4, d:5},
-				grid = o.grid,
-				iterator;
-	
-				if ( !grid ) {
-					if ( $kids.length <= 5 ) {
-						for ( var letter in gridCols ) {
-							if ( gridCols[ letter ] === $kids.length ) {
-								grid = letter;
-							}
-						}
-					} else {
-						grid = "a";
+		if ( !grid ) {
+			if ( $kids.length <= 5 ) {
+				for ( var letter in gridCols ) {
+					if ( gridCols[ letter ] === $kids.length ) {
+						grid = letter;
 					}
 				}
-				iterator = gridCols[grid];
-	
-			$this.addClass( "ui-grid-" + grid );
-	
-			$kids.filter( ":nth-child(" + iterator + "n+1)" ).addClass( "ui-block-a" );
-	
-			if ( iterator > 1 ) {
-				$kids.filter( ":nth-child(" + iterator + "n+2)" ).addClass( "ui-block-b" );
+			} else {
+				grid = "a";
 			}
-			if ( iterator > 2 ) {
-				$kids.filter( ":nth-child(3n+3)" ).addClass( "ui-block-c" );
-			}
-			if ( iterator > 3 ) {
-				$kids.filter( ":nth-child(4n+4)" ).addClass( "ui-block-d" );
-			}
-			if ( iterator > 4 ) {
-				$kids.filter( ":nth-child(5n+5)" ).addClass( "ui-block-e" );
-			}
-		};
+		}
+		iterator = gridCols[grid];
+
+		$this.addClass( "ui-grid-" + grid );
+
+		$kids.filter( ":nth-child(" + iterator + "n+1)" ).addClass( "ui-block-a" );
+
+		if ( iterator > 1 ) {
+			$kids.filter( ":nth-child(" + iterator + "n+2)" ).addClass( "ui-block-b" );
+		}
+		if ( iterator > 2 ) {
+			$kids.filter( ":nth-child(3n+3)" ).addClass( "ui-block-c" );
+		}
+		if ( iterator > 3 ) {
+			$kids.filter( ":nth-child(4n+4)" ).addClass( "ui-block-d" );
+		}
+		if ( iterator > 4 ) {
+			$kids.filter( ":nth-child(5n+5)" ).addClass( "ui-block-e" );
+		}
 	});
 };
 })( jQuery );
