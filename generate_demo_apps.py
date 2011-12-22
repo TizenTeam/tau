29a5b43356a74e688991a329b80c8f572a891a9b
Koeun Choi <koeun.choi@samsung.com>
2011-12-22 18:22:26 +0900

    [tools] Add "generate_demo_apps.py" to generate wgt file automatically.  - How to use  1. install python if you don't have.     # apt-get install python  2. run generate_demo_apps.py     # ./generate_demo_apps.py  It will automatically make wgt files from all demo app in demos directory.
    Change-Id: Ib7e188b558b583ef2bfd79cb72212864ef42aaba

---
+++ generate_demo_apps.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# author koeun.choi@samsung.com
+# This script processes packaging for all directories under "demos" into web-apps.
+
+import os, sys, shutil
+
+def changeIndexFile():
+	filename = "index.html"
+	f = open(filename)
+	before = f.read()
+	f.close()
+	after = before.replace("../../build/web-ui-fw", "web-ui-fw")
+	f = open(filename, "w")
+	f.write(after)
+	f.close()
+
+
+def addConfigFileAndIcon(webAppName):
+	# config.xml file
+	shutil.copy("../../src/template/wac/config.xml.in", "config.xml")
+	f = open("config.xml")
+	config = f.read()
+	f.close()
+	config = config.replace("templateID", webAppName)
+	config = config.replace("@APP_NAME@", webAppName)
+	f = open("config.xml", "w")
+	f.write(config)
+	f.close()
+	# icon.png
+	shutil.copy("../../src/template/wac/icon.png", "icon.png")
+
+def makeWebAppPackage(directory):
+	os.chdir(directory)
+	webAppName = directory.replace("pkged-demo-apps/", "")
+	changeIndexFile()
+	# copy web-ui-fw lib
+	shutil.copytree("../../build/web-ui-fw", "web-ui-fw")
+	#add meta files for packaging
+	addConfigFileAndIcon(webAppName)
+	# zip dirs...
+	print "making " + webAppName + ".wgt ...."
+	os.system("zip -r ../" + webAppName + ".wgt ." )
+	os.chdir("../")
+
+
+def search(dirname):
+	flist = os.listdir(dirname)
+	os.chdir(dirname)
+	for f in flist:
+		print "current path: " + os.getcwd()
+		if os.path.isdir(f):
+			print "web app name :" + f
+			makeWebAppPackage(f)
+		else:
+		 ext = os.path.splitext(f)[-1]
+		 if ext != ".wgt":
+			print "not directory :" + f
+
+		# remove redundant generated files
+		print "Remove redundant generated files while processing...."
+		os.system("rm -rf " + f)
+
+print "##############################"
+print "It will generate demo apps(wgt) packages under demos directory"
+
+os.system("rm -rf pkged-demo-apps")
+shutil.copytree("demos", "pkged-demo-apps")
+search("pkged-demo-apps")
+
+print ""
+print "Packaged demo apps are generated successfully in [pkged-demo-apps]"
+print "##############################"
