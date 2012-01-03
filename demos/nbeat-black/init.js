8d9a6d4da1f6cc57548c29a12f16c7895d7c700f
Minkyu Kang <mk7.kang@samsung.com>
2012-01-03 17:39:51 +0900

    smallpopup: fix text format as UI guide
    Change-Id: Ie8008df09cba54d64e0c55aafd8be70d625c08e4

--- demos/nbeat-black/init.js
+++ demos/nbeat-black/init.js
@@ -108,7 +108,7 @@ $(document).bind("pagecreate", function () {
 	});
 
 	$('#selectioninfo-demo').bind('vmouseup', function (e) {
-		$('#smallpopup_selectioninfo').attr("data-text1", $("#dayselector1").find(".ui-checkbox-on").length);
+		$('#smallpopup_selectioninfo').attr("data-text", $("#dayselector1").find(".ui-checkbox-on").length + " items are selected");
 		$('#smallpopup_selectioninfo').smallpopup('show');
 	});
 
