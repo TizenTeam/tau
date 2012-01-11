e2649f09254bcf58951e543c1bb8e309eeffdc2e
Noha Park <noha.park@samsung.com>
2012-01-11 12:10:16 +0900

    modify blue/gray theme config.less
    Change-Id: I9423fae5c91b9b04b09845fc886a83c18effa066

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ sbs -eo make clean
 sbs -eo make
 sudo rm -rf /var/www/webui/*
 sudo cp -rf * /var/www/webui
-google-chrome --allow-file-access-from-files demos/nbeat-black/index.html
+google-chrome --allow-file-access-from-files demos/nbeat-gray/index.html
 
