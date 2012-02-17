8165151f2e41512b89bbc50df8209a4822a17d29
Koeun Choi <koeun.choi@samsung.com>
2012-02-17 13:25:55 +0900

    remove reduandant files
    Change-Id: Ic670c7dadc0f9610bef736bfd1ab5dfe3aa05049

--- src/jqm/docs/pages/theme.js
+++
@@ -1,7 +0,0 @@
-(function( $, undefined ) {
-
-$.mobile.page.prototype.options.backBtnTheme	= null;
-$.mobile.page.prototype.options.headerTheme		= null;
-$.mobile.page.prototype.options.footerTheme		= null;
-$.mobile.page.prototype.options.contentTheme	= null;
-})(jQuery);
