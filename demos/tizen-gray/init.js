07218e1a3e358148dafb3754fe2e1b94048948a2
Youmin Ha <youmin.ha@samsung.com>
2012-03-27 20:10:38 +0900

    Change scaling algorithm: support two modes, 320 mode and 720 mode
    Change-Id: I40f772228b5d63bc1f5f6029a731985f4caeb0d1

--- demos/tizen-gray/init.js
+++ demos/tizen-gray/init.js
@@ -234,4 +234,5 @@ $(document).ready( function () {
 	// NOTE: Globalize.* functions must be run after docoument ready.
 	$('#current_date').html(Globalize.culture().name + " -- " +
 				Globalize.format( new Date(), "F" ));
+	$('#html_font_size').html('html font size:' + $('html').css('font-size'));
 });
