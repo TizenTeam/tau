13e99c5c22afcd3919199ed311839aee16bcf444
Minkyu Kang <mk7.kang@samsung.com>
2012-05-15 14:27:18 +0900

    unit test: update tests.js
    Change-Id: I2b0969758cf7e7b5d33bbb72a2f265a0887d9a45

--- tests/unit-tests/tests.js
+++ tests/unit-tests/tests.js
@@ -1,22 +1,16 @@
 var TESTS = {
 	"testPages":[
 		// Put your test here
-		"pagecontrol",
-/*
-        "singleimagedisplay",
-		"swipelist",
-		"processingcircle",
-		"autodividers",
-		"listviewcontrols",
-		"dayselector",
-		"optionheader",
-		"processingbar",
-*/
-		"progressbar",
 		"popupwindow",
 		"slider",
 		"notification",
+		"imageslider",
+		"pagecontrol",
+		"datetimepicker",
 		"nocontents",
-		"imageslider"
+		"handler",
+		"multibuttonentry",
+		"multimediaview",
+		"virtualgrid",
 	]
 };
