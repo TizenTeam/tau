05614215ccbf4453e4d1b9c634d4fe7e744d5883
wongi11.lee <wongi11.lee@samsung.com>
2012-05-29 17:42:17 +0900

    Update virtual list unit test. fix Extendable list text.
    Change-Id: I5eb7e7a8d28607ce1d73dcece66e02f1584b7273

--- tests/unit-tests/virtuallist/virtuallist-tests.js
+++ tests/unit-tests/virtuallist/virtuallist-tests.js
@@ -6,99 +6,98 @@
 
 $( document ).ready( function () {
 
-	setTimeout( function (){
-		module( "Virtual List");
+	module( "Virtual List");
 
-		function startVirtualListTest(){
-			var $vlContainer = $( "ul.ui-virtual-list-container" ),
-				$vlElements = $( "ul.ui-virtual-list-container li" ),
-				vlHeight = $vlContainer.css( "height" ),
-				vlOptions = $( "#virtuallist-normal_1line_ul" ).virtuallistview( "option" );
-			console.dir( vlOptions );
+	function startVirtualListTest(){
+		var $vlContainer = $( "ul.ui-virtual-list-container" ),
+			$vlElements = $( "ul.ui-virtual-list-container li" ),
+			vlHeight = $vlContainer.css( "height" ),
+			vlOptions = $( "#virtuallist-normal_1line_ul" ).virtuallistview( "option" );
 
-			test( "Virtual list test", function () {
-				/* Initialize and create method */
+		test( "Virtual list test", function () {
+			/* Initialize and create method */
+			ok( $vlContainer );
+			equal( $vlElements.length, 100 );
+			ok( parseInt( vlHeight, 10 ) > 3000 );
+
+			/* Options */
+			equal( vlOptions.id, "#virtuallist-normal_1line_ul" );
+			equal( vlOptions.childSelector, " li" );
+			equal( vlOptions.dbtable, "JSON_DATA" );
+			equal( vlOptions.template, "tmp-1line" );
+			equal( vlOptions.row, 100 );
+			equal( vlOptions.dbkey, false );
+			equal( vlOptions.scrollview, true );
+
+			ok ( ( function () {
+				var i = 0,
+					newJSON = new Array(),
+					newItem,
+					firstLI,
+					result = true;
+
+				/* make short JSON array */
+				for ( i = 0; i < 200; i++ ) {
+					newJSON.push( window.JSON_DATA[ ( i + 100 ) ] );
+				}
+
+				/* Call recreate */
+				$( "#virtuallist-normal_1line_ul" ).virtuallistview( "recreate", newJSON );
+
+				$vlContainer = $( "ul.ui-virtual-list-container" );
+				$vlElements = $( "ul.ui-virtual-list-container li" );
+
+				/* Check new List */
 				ok( $vlContainer );
 				equal( $vlElements.length, 100 );
 				ok( parseInt( vlHeight, 10 ) > 3000 );
 
-				/* Options */
-				equal( vlOptions.id, "#virtuallist-normal_1line_ul" );
-				equal( vlOptions.childSelector, " li" );
-				equal( vlOptions.dbtable, "JSON_DATA" );
-				equal( vlOptions.template, "tmp-1line" );
-				equal( vlOptions.row, 100 );
-				equal( vlOptions.dbkey, false );
-				equal( vlOptions.scrollview, true );
+				newItem = window.JSON_DATA[ 100 ];
 
-				ok ( ( function () {
-					var i = 0,
-						newJSON = new Array(),
-						newItem,
-						firstLI,
-						result = true;
+				firstLI = $( "ul.ui-virtual-list-container li:first" );
 
-					/* make short JSON array */ 
-					for ( i = 0; i < 200; i++ ) {
-						newJSON.push( window.JSON_DATA[ ( i + 100 ) ] );
-					}
+				try {
+					equal( newItem.NAME, $( firstLI ).find( "span.ui-li-text-main" ).text() );
+				} catch ( exception ) {
+					console.log( exception );
+					return false;
+				}
 
-					/* Call recreate */ 
-					$( "#virtuallist-normal_1line_ul" ).virtuallistview( "recreate", newJSON );
+				return true;
+			}() ), "recreate()" );
 
-					$vlContainer = $( "ul.ui-virtual-list-container" );
-					$vlElements = $( "ul.ui-virtual-list-container li" );
+			/* Destroy method */
+			ok ( ( function () {
+				/* Call destroy */
+				$( "#virtuallist-normal_1line_ul" ).virtuallistview( "destroy" );
 
-					/* Check new List */ 
-					ok( $vlContainer );
-					equal( $vlElements.length, 100 );
-					ok( parseInt( vlHeight, 10 ) > 3000 );
+				destoyedVlElements = $( "ul.ui-virtual-list-container li" );
+				console.log( destoyedVlElements.length );
 
-					newItem = window.JSON_DATA[ 100 ];
+				try {
+					equal ( destoyedVlElements.length, 0 );
+				} catch ( exception ) {
+					console.log( "destroy : " + exception );
+					return false;
+				}
+				return true;
+			}() ), "destroy()" );
+		} );
+	}
 
-					firstLI = $( "ul.ui-virtual-list-container li:first" );
-
-					try {
-						equal( newItem.NAME, $( firstLI ).find( "span.ui-li-text-main" ).text() );
-					} catch ( exception ) {
-						console.log( exception );
-						return false;
-					}
-
-					return true;
-				}() ), "recreate()" );
-
-				/* Destroy method */
-				ok ( ( function () {
-					/* Call destroy */
-					$( "#virtuallist-normal_1line_ul" ).virtuallistview( "destroy" );
-
-					destoyedVlElements = $( "ul.ui-virtual-list-container li" );
-					console.log( destoyedVlElements.length );
-
-					try {
-						equal ( destoyedVlElements.length, 0 );
-					} catch ( exception ) {
-						console.log( "destroy : " + exception );
-						return false;
-					}
-					return true;
-				}() ), "destroy()" );
-			} );
-		}
+	/* Load Dummy Data and Init Virtual List widget*/
+	if ( window.JSON_DATA ) {
+		$( "ul" ).filter( function () {
+			return $( this ).data( "role" ) == "virtuallistview";
+		} ).addClass( "vlLoadSuccess" );
 
 		// trigger pagecreate
 		$( "#virtuallist-unit-test" ).page();
 
-		/* Load Dummy Data and Init Virtual List widget*/
-		$.getScript( "../../../demos/tizen-gray/widgets/list/virtuallist-db-demo.js", function ( data, textStatus ) {
-			$( "ul" ).filter( function () {
-				return $( this ).data( "role" ) == "virtuallistview";
-			} ).addClass( "vlLoadSuccess" );
+		$( "ul.ui-virtual-list-container" ).virtuallistview( "create" );
 
-			$( "ul.ui-virtual-list-container" ).virtuallistview( "create" );
-
-			startVirtualListTest();
-		} );
-	}, 1000 );
+		startVirtualListTest();
+	} else {
+		console.log ( "Virtual List Init Fail." );
+	}
 } );
