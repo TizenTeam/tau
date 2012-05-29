2a1272bf68e1dddd6c9724cf770ae2c6fde3670d
Koeun Choi <koeun.choi@samsung.com>
2012-05-29 13:39:00 +0900

    Revert "Squashed commit of the following:"
    This reverts commit 24bc39ef8556d891fe030ed1804636f6ee7d0cd3.

--- tests/performance/monitor/paintmonitor.js
+++
@@ -1,9 +0,0 @@
-function RepaintMonitor() {
-	this.used = 0;
-	this.notify = function ( event ) {
-		if ( event.type == "Paint" ) {
-			this.used += event.data.width * event.data.height;
-			console.log( "Repainted Area: " + this.used );
-		}
-	};
-};
