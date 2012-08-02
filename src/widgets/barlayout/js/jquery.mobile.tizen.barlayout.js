c4b016b42f12d737908e51b7b8bd2ddcafb86285
Koeun Choi <koeun.choi@samsung.com>
2012-08-02 21:02:10 +0900

    Revert "Fix: back button does not work when long press." RevertReason - Focus ring does not work properly.   Revert this commit until focus ring is fixed.
    This reverts commit f87a836c2123b415f30598022221dae9a9ee83b1.

--- src/widgets/barlayout/js/jquery.mobile.tizen.barlayout.js
+++ src/widgets/barlayout/js/jquery.mobile.tizen.barlayout.js
@@ -79,20 +79,9 @@
 					}
 				}
 
-				// block long press default behavior of webRuntime
-				backBtn.bind( "touchstart", function( event ) {
-					event.preventDefault();
-				});
-				backBtn.bind( "vclick", function( event ) {
-					if( $.support.touch ) {
-						$( this ).trigger( "click" );
-					}
-				});
-
-				/* jQM 1.1.0 do not need this code
-				 * navigation.js control whole back button  */
-				/*
-				backBtn.bind( "vclick", function( event ) {
+/* jQM 1.1.0 do not need this code
+* navigation.js control whote back button  */
+/*				backBtn.bind( "vclick", function( event ) {
 					window.history.back();
 				});*/
 		},
