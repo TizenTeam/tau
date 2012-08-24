1325d485b276cf82e383a9eacb230c6f90a9f91a
Youmin Ha <youmin.ha@samsung.com>
2012-08-24 15:40:01 +0900

    Clean-up propretary winsets from src tree

--- tools/cleanup-src.sh
+++ tools/cleanup-src.sh
@@ -22,6 +22,21 @@ path_to_be_removed=( \
 
 cd `dirname $0`/../
 for p in "${path_to_be_removed[@]}"; do
-	echo $p
+	echo "remove $p"
 	rm -rf $p
+done
+
+# Remove propretary winsets from demo
+sed -i -n '1h;1!H;${;g;s/<!-- START_PROPRIETARY -->.*<!-- END_PROPRIETARY -->//g;p;}' demos/tizen-winsets/index.html
+
+# clean-up propretary winsets
+prop_widgets=( mapview )
+
+for w in "${prop_widgets[@]}"; do
+	echo "Remove widget: $w"
+	rm -rf src/widgets/${w}
+	find demos/tizen-winsets/ -name "*${w}*" -exec rm -rf {} \;
+	find src/themes/ -name "*${w}*" -exec rm -rf {} \;
+	grep -r "$w" src/themes/ | cut -f1 -d: | xargs sed -i -e "/${w}/d"
+
 done
