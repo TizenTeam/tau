6b9d5c3398c233cd2bf2c29b9aa4a58d8a75fbf7
heeju Joo <heeju.joo@samsung.com>
2014-01-08 18:43:00 +0900

    B2 basic demo: modify app exit method
    - window.close() not used, so this patch changed method
    - tizen.application.getCurrentApplication().exit()
    
    Change-Id: Ib175aea5f4337546dd4d4cfccd02c2e1a7b5180b

--- standalone/demos/app.js
+++ standalone/demos/app.js
@@ -4,7 +4,7 @@
 			var page = document.getElementsByClassName( 'ui-page-active' )[0],
 				pageid = page ? page.id : "";
 			if( pageid === "main" ) {
-				window.close();
+				tizen.application.getCurrentApplication().exit();
 			} else {
 				window.history.back();
 			}
