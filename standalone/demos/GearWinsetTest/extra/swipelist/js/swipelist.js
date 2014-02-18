31a14b44b2aaaee1f4f5b21c6dc0d352d8d7d16f
Maciej Urbanski <m.urbanski@samsung.com>
2014-02-18 10:00:39 +0100

    [FIX] In swipe add support to hide element after stop touch outside swipe (GSDK_SE-1425)
    Change-Id: Id7506ba12782c14b1ebc9697b846e96a1c1b8dcb

--- standalone/demos/GearWinsetTest/extra/swipelist/js/swipelist.js
+++ standalone/demos/GearWinsetTest/extra/swipelist/js/swipelist.js
@@ -21,6 +21,13 @@ function SwipeList( liElem, callElem, messageElem ) {
 	this.init( liElem, callElem, messageElem );
 }
 
+function detectLiTarget (target) {
+	while (target && target.tagName !== 'LI') {
+		target = target.parentNode;
+	}
+	return target;
+}
+
 SwipeList.prototype = {
 	init: function( liElem, callElem, messageElem ) {
 		var page;
@@ -55,81 +62,83 @@ SwipeList.prototype = {
 		callBPX = self._genlistCallStyle["background-position-x"],
 		messageBPX = self._genlistMessageStyle["background-position-x"];
 
-		for( var i=0; i < self._genlistLi.length; i++ ){
-			self._genlistLi[i].addEventListener( "touchstart", function( e ) {
-				var touches = e.touches;
+		document.addEventListener( "touchstart", function( e ) {
+			var touches = e.touches;
+			if (detectLiTarget(e.target)) {
 				startX = touches[0].pageX;
 				dragging = true;
-			});
-			self._genlistLi[i].addEventListener( "touchmove", function( e ) {
-				var touches = e.touches,
+			}
+		});
+		document.addEventListener( "touchmove", function( e ) {
+			var touches = e.touches,
+				target = detectLiTarget(e.target),
 				sx = touches[0].pageX - startX,
-				top = this.offsetTop - self._genlistCall.parentNode.scrollTop + "px";
-				if( dragging ) {
-					if ( sx > self.options.threshold ){
-						// appear call scroller
-						var bpx = self._genlistCallStyle["background-position-x"];
-						self._genlistCallStyle["top"] = top;
-						self._genlistCallStyle["display"] = "block";
-						self._genlistCallStyle["background-position-x"] = parseInt( callBPX ) + sx + "px";
+				top;
+			if( dragging && target) {
+				top = target.offsetTop - self._genlistCall.parentNode.scrollTop + "px";
+				if ( sx > self.options.threshold ){
+					// appear call scroller
+					var bpx = self._genlistCallStyle["background-position-x"];
+					self._genlistCallStyle["top"] = top;
+					self._genlistCallStyle["display"] = "block";
+					self._genlistCallStyle["background-position-x"] = parseInt( callBPX ) + sx + "px";
 
-						e.preventDefault();
-					} else if ( sx < -self.options.threshold ) {
-						var bpx = self._genlistMessageStyle["background-position-x"];
-						self._genlistMessageStyle["top"] = top;
-						self._genlistMessageStyle["display"] = "block";
-						self._genlistMessageStyle["background-position-x"] = parseInt( messageBPX ) + sx + "px";
+					e.preventDefault();
+				} else if ( sx < -self.options.threshold ) {
+					var bpx = self._genlistMessageStyle["background-position-x"];
+					self._genlistMessageStyle["top"] = top;
+					self._genlistMessageStyle["display"] = "block";
+					self._genlistMessageStyle["background-position-x"] = parseInt( messageBPX ) + sx + "px";
 
-						e.preventDefault();
+					e.preventDefault();
+				}
+			}
+		});
+
+		document.addEventListener( "touchend", function( e ) {
+			var callBPX = self._genlistCallStyle["background-position-x"],
+			messageBPX = self._genlistMessageStyle["background-position-x"],
+			interval;
+
+			if( parseInt( callBPX ) > -200 ) {
+				// animate call background x position
+				var i = parseInt( callBPX );
+
+				(function animate(){
+					if ( i < 0 ){
+						self._genlistCallStyle["background-position-x"] = i + "px"
+						i+=20;
+						webkitRequestAnimationFrame( animate );
+					} else {
+						self._genlistCallStyle["background-position-x"] = "-400px";
+						self._genlistCall.style.display = "none";
+						// fired custom event
+						self._genlistCall.dispatchEvent( calling );
 					}
-				}
-			});
-			self._genlistLi[i].addEventListener( "touchend", function( e ) {
-				var callBPX = self._genlistCallStyle["background-position-x"],
-				messageBPX = self._genlistMessageStyle["background-position-x"],
-				interval;
+				})();
 
-				if( parseInt( callBPX ) > -200 ) {
-					// animate call background x position
-					var i = parseInt( callBPX );
+			} else if( parseInt( messageBPX ) < -200 ) {
+				// animate message background x position
+				var i = parseInt( messageBPX );
 
-					(function animate(){
-						if ( i < 0 ){
-							self._genlistCallStyle["background-position-x"] = i + "px"
-							i+=20;
-							webkitRequestAnimationFrame( animate );
-						} else {
-							self._genlistCallStyle["background-position-x"] = "-400px";
-							self._genlistCall.style.display = "none";
-							// fired custom event
-							self._genlistCall.dispatchEvent( calling );
-						}
-					})();
+				(function animate(){
+					if ( i > -400 ){
+						self._genlistMessageStyle["background-position-x"] = i + "px"
+						i-=20;
+						webkitRequestAnimationFrame( animate );
+					} else {
+						self._genlistMessageStyle["background-position-x"] = "-20px";
+						self._genlistMessage.style.display = "none";
+						// fired custom event
+						self._genlistCall.dispatchEvent( messaging );
+					}
+				})();
+			} else {
+				self._genlistCall.style.display = "none";
+				self._genlistMessage.style.display = "none";
+			}
 
-				} else if( parseInt( messageBPX ) < -200 ) {
-					// animate message background x position
-					var i = parseInt( messageBPX );
-
-					(function animate(){
-						if ( i > -400 ){
-							self._genlistMessageStyle["background-position-x"] = i + "px"
-							i-=20;
-							webkitRequestAnimationFrame( animate );
-						} else {
-							self._genlistMessageStyle["background-position-x"] = "-20px";
-							self._genlistMessage.style.display = "none";
-							// fired custom event
-							self._genlistCall.dispatchEvent( messaging );
-						}
-					})();
-				} else {
-					self._genlistCall.style.display = "none";
-					self._genlistMessage.style.display = "none";
-				}
-
-				dragging = false;
-
-			});
-		}
+			dragging = false;
+		});
 	}
 }
