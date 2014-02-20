36412ec686e66bdcf5b5d9165373a71311af1169
Youmin Ha <youmin.ha@samsung.com>
2014-02-20 19:09:05 +0900

    virtuallist: Fix exporting namespace to gear.ui
    Change-Id: I963a50c87fe19c816a7a769d595039f58c400a37

--- standalone/src/ej/virtuallist.js
+++ standalone/src/ej/virtuallist.js
@@ -11,6 +11,12 @@
 		],
 		function (require) {
 			require(["./init"], function () {
+//>>excludeEnd("ejBuildExclude");
+(function() {
+var newGear = gear.noConflict();
+gear.ui.VirtualListview = newGear.ui.VirtualListview.bind(newGear.ui);
+}());
+//>>excludeStart("ejBuildExclude", pragmas.ejBuildExclude);
 				return true;
 			});
 		}
