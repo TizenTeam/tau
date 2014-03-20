ec14b039896016a833f8d72d065b6b792b252abf
Youmin Ha <youmin.ha@samsung.com>
2014-03-20 14:36:00 +0900

    Change widget name
    Change-Id: I95ef65d8c95821ccfc484ec3ab0f22b5b2bb4de9

--- standalone/demos/debugGearWinsetTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd GearWinsetTest; rm -f ../GearWinsetTest.wgt; zip -r ../GearWinsetTest.wgt .; sdb root on; sdb shell wrt-installer -un 1234567890.gearwinsettest; sdb install ../GearWinsetTest.wgt; sdb shell wrt-launcher -ds 1234567890.gearwinsettest -t 30 | grep port: | cut -f2 -d\ | xargs -I{} sh -c "sdb forward tcp:55555 tcp:{}"; cd ..; google-chrome http://127.0.0.1:55555/inspector.html?page=1;
-
