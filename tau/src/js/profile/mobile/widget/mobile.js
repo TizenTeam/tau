f66f0916001b0d2df14e2ebc344bd7bcf05fc24f
Hyunkook, Cho <hk0713.cho@samsung.com>
2014-05-13 20:18:34 +0900

    [Cleanup] create mobile widget namespace.
    Change-Id: Iad9a703d8fafdc9f6243c532984262243c1bacde

---
+++ tau/src/js/profile/mobile/widget/mobile.js
@@ -0,0 +1,25 @@
+/*global window, define, ns */
+/*
+* Copyright (c) 2010 - 2014 Samsung Electronics Co., Ltd.
+* License : MIT License V2
+*/
+/*
+ * @author Maciej Urbanski <m.urbanski@samsung.com>
+ */
+(function (window, ns) {
+	"use strict";
+	//>>excludeStart("tauBuildExclude", pragmas.tauBuildExclude);
+	define(
+		[
+			"../../../core/widget"
+		],
+		function () {
+			//>>excludeEnd("tauBuildExclude");
+			/** @namespace ns.widget.mobile */
+			ns.widget.mobile = ns.widget.mobile || {};
+			//>>excludeStart("tauBuildExclude", pragmas.tauBuildExclude);
+			return ns.widget.mobile;
+		}
+	);
+	//>>excludeEnd("tauBuildExclude");
+}(window, ns));
