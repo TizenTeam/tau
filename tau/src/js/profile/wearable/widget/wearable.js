c1728ed203ac268c1763ad2d7b612a4030bcbab3
Hyunkook, Cho <hk0713.cho@samsung.com>
2014-05-13 19:15:25 +0900

    [Cleanup] create wearable widget namespace.
    Change-Id: Iaeb01f0ca3caa1d2ec33a1fd90f5ef549e060517

---
+++ tau/src/js/profile/wearable/widget/wearable.js
@@ -0,0 +1,25 @@
+/*global window, define, ns */
+/*
+* Copyright (c) 2010 - 2014 Samsung Electronics Co., Ltd.
+* License : MIT License V2
+*/
+/*
+ * @author Maciej Urbanski <m.urbanski@samsung.com>
+ */
+(function (window, ns) {
+	"use strict";
+	//>>excludeStart("tauBuildExclude", pragmas.tauBuildExclude);
+	define(
+		[
+			"../../../core/widget"
+		],
+		function () {
+			//>>excludeEnd("tauBuildExclude");
+			/** @namespace ns.widget.wearable */
+			ns.widget.wearable = ns.widget.wearable || {};
+			//>>excludeStart("tauBuildExclude", pragmas.tauBuildExclude);
+			return ns.widget.wearable;
+		}
+	);
+	//>>excludeEnd("tauBuildExclude");
+}(window, ns));
