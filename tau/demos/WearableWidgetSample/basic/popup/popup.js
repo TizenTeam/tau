d9db6d4cbccdc52897257b43c5e020d14f8d74b7
Hyunkook, Cho <hk0713.cho@samsung.com>
2014-05-19 17:39:47 +0900

    [Cleanup] cleanup tau library path in WearableWidgetSample.
    Change-Id: I25381228ab5c2d4be21710d5b7aadcf86e6482df

--- tau/demos/WearableWidgetSample/basic/popup/popup.js
+++ tau/demos/WearableWidgetSample/basic/popup/popup.js
@@ -1,13 +1,18 @@
-(function (ui) {
-	var closePopup = ui.closePopup.bind(ui);
+(function() {
+	var page = document.getElementById("popupPage");
 
-	document.getElementById('1btnPopup-cancel').addEventListener('click', closePopup, false);
-	document.getElementById('2btnPopup-cancel').addEventListener('click', closePopup, false);
-	document.getElementById('2btnPopup-ok').addEventListener('click', closePopup, false);
-	document.getElementById('radioPopup-close').addEventListener('click', closePopup, false);
-	document.getElementById('checkPopup-close').addEventListener('click', closePopup, false);
-	document.getElementById('listPopup-cancel').addEventListener('click', closePopup, false);
-	document.getElementById('listNoTitlePopup-cancel').addEventListener('click', closePopup, false);
-	document.querySelector('#listNoBtnPopup .ui-listview').addEventListener('click', closePopup, false);
-	document.querySelector('#listNoTitleNoBtnPopup .ui-listview').addEventListener('click', closePopup, false);
-})(window.tau);
+	page.addEventListener("pageshow", function(ev) {
+		var closePopup = tau.closePopup.bind(tau);
+
+		document.getElementById('1btnPopup-cancel').addEventListener('click', closePopup, false);
+		document.getElementById('2btnPopup-cancel').addEventListener('click', closePopup, false);
+		document.getElementById('2btnPopup-ok').addEventListener('click', closePopup, false);
+		document.getElementById('radioPopup-close').addEventListener('click', closePopup, false);
+		document.getElementById('checkPopup-close').addEventListener('click', closePopup, false);
+		document.getElementById('listPopup-cancel').addEventListener('click', closePopup, false);
+		document.getElementById('listNoTitlePopup-cancel').addEventListener('click', closePopup, false);
+		document.querySelector('#listNoBtnPopup .ui-listview').addEventListener('click', closePopup, false);
+		document.querySelector('#listNoTitleNoBtnPopup .ui-listview').addEventListener('click', closePopup, false);
+	}, false);
+
+}());
