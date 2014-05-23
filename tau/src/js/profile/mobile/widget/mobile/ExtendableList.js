d1f978a8c6758bb1e48418c795e3930eb2d757ee
junhyeonLee <juneh.lee@samsung.com>
2014-05-23 14:25:13 +0900

    License review for Tizen 2.3 source open
    Due to the open Tizen 2.3,
    LICENSE, Boilerplate and AUTHOR files are updated.
    
    As MIT license is not in use, all files go under Flora license.
    COPYING file is updated.
    AUTHOR file is added.
    
    Change-Id: Ib075926c50d9a5bb3cdc0b525a72c02f416f8281

--- tau/src/js/profile/mobile/widget/mobile/ExtendableList.js
+++ tau/src/js/profile/mobile/widget/mobile/ExtendableList.js
@@ -1,17 +1,28 @@
 /*global window, define, ns, $ */
-/*jslint nomen: true, white: true, plusplus: true*/
 /*
- * Copyright (c) 2010 - 2014 Samsung Electronics Co., Ltd.
- * License : MIT License V2
- */
+* Copyright (c) 2013 - 2014 Samsung Electronics Co., Ltd
+*
+* Licensed under the Flora License, Version 1.1 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://floralicense.org/license/
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/*jslint nomen: true, white: true, plusplus: true*/
 /**
- * #Extendable List Widget
- * The extendable list is used to display a list of data elements that can be extended.
- *
- * @class ns.widget.ExtendableList
- * @extend ns.widget.BaseWidget
- * @author Michał Szepielak <m.szepielak@samsung.com>
- */
+* #Extendable List Widget
+* The extendable list is used to display a list of data elements that can be extended.
+*
+* @class ns.widget.ExtendableList
+* @extend ns.widget.BaseWidget
+* @author Michał Szepielak <m.szepielak@samsung.com>
+*/
 (function(document, ns) {
 	"use strict";
 	//>>excludeStart("tauBuildExclude", pragmas.tauBuildExclude);
