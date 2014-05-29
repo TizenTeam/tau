695e83dfd22e608067f243ac17de8f31ce42d56b
Krzysztof Antoszek <k.antoszek@samsung.com>
2014-05-29 14:37:30 +0200

    tests: pagecontainer tests fixes
    * fixed wrong test case conditions
    * shuffled init code around to ensure all variables are set properly
    
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

--- tau/tests/js/jqm/router/pagecontainer.js
+++ tau/tests/js/jqm/router/pagecontainer.js
@@ -3,15 +3,15 @@ module("ej.jqm.router");
 asyncTest ( "pageContainer" , 5, function () {
 	var pageContainer = document.getElementById('pageContainer1');
 	ok(!document.body.classList.contains('ui-mobile-viewport'), 'body not contains ui-mobile-viewport');
-	ok(!document.body.classList.contains('ui-overlay-s'), 'body not contains overlay-s');
 	ok(pageContainer.classList.contains('ui-mobile-viewport'), 'pageContainer contains ui-mobile-viewport');
-	ok(pageContainer.classList.contains('ui-overlay-s'), 'pageContainer contains overlay-s');
-	setTimeout(function() {
+	ok(document.body.className.indexOf('ui-overlay') === -1, 'body not contains overlay');
+	ok(pageContainer.className.indexOf('ui-overlay') > -1, 'pageContainer contains overlay');
+	window.setTimeout(function () {
 		start();
 	}, 1000);
-	$('#pageContainer1').bind('pagechange', function(event) {
+	$(pageContainer).bind('pagechange', function(event) {
 		ok(event.target, 'call changepage');
 	});
-	document.getElementById('btn1').click();
-	document.getElementById('btn2').click();
+	document.getElementById("btn1").click();
+	document.getElementById("btn2").click();
 });
