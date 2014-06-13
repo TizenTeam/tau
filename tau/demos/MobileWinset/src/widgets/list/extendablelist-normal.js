5b22cc41a1b236dca52f9da90a65eb50af6b0042
Michał Szepielak <m.szepielak@samsung.com>
2014-06-13 11:31:15 +0200

    MobileWinset: fix showing extendable list
    Change-Id: I7bcf4d20d4de8b9e940c865af4953c1060500791
    Signed-off-by: Michał Szepielak <m.szepielak@samsung.com>

--- tau/demos/MobileWinset/src/widgets/list/extendablelist-normal.js
+++ tau/demos/MobileWinset/src/widgets/list/extendablelist-normal.js
@@ -6,25 +6,28 @@ $("#genlist_extendable_normal_page").one("pagecreate", function ( el ) {
 				dataLength: JSON_DATA.length,
 				//Set buffer size
 				bufferSize: 50,
-				listItemUpdater: function(elListItem, newIndex) {
+			// Set list item updater
+			listItemUpdater: function (processedIndex, listItem) {
 					//TODO: Update listitem here
-					var data =  JSON_DATA[newIndex];
-					elListItem.innerHTML = '<span class="ui-li-text-main">' + data.NAME + '</span>';
+					var data = JSON_DATA[processedIndex];
+					listItem.innerHTML = '<span class="ui-li-text-main">' + data.NAME + '</span>';
 				},
 				listItemLoader: function(elListItem, numMoreItems) {
 					// @TODO tau._export should not be used in application development,
 					// @TODO but it's currently the only way of fixing this issue
-					var engine = tau._export.engine,
-						loader = engine.getBinding(document.getElementById('extendableListButton'));
+
+					var loader = document.getElementById('extendableListButton');
 
 					if (!loader) {
 						loader = document.createElement('div');
 						loader.id = 'extendableListButton';
 						elListItem.appendChild(loader);
-						loader = engine.instanceWidget(loader, 'Button');
-					}
 
+					}
+					loader = tau.widget.Button(loader);
+					// @TODO: quick fix for button, please fix button widget
 					loader.element.innerHTML = 'Load ' + numMoreItems + ' more items';
+					// loader.option('value', 'Load ' + numMoreItems + ' more items');
 				}
 			};
 
