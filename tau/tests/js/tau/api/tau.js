228515c0400c8ec9af6515ad00ac87614147b35b
Piotr Karny <p.karny@samsung.com>
2014-08-12 10:44:47 +0200

    Tests: API tests for frameworkData object
    Change-Id: I7d09eec1446ea47dbce8dc631eebeba7d1a79868
    Signed-off-by: Piotr Karny <p.karny@samsung.com>

--- tau/tests/js/tau/api/tau.js
+++ tau/tests/js/tau/api/tau.js
@@ -1,6 +1,10 @@
-module("API tau");
+(function (tau){
+	"use strict";
 
-test ( "API tau" , function () {
-	equal(typeof tau, 'object', 'Class tau exists');
-	equal(typeof tau.noConflict, 'function', 'Class tau.noConflict exists');
-});
+	module("API tau");
+
+	test ( "API tau" , function () {
+		equal(typeof tau, 'object', 'Class tau exists');
+		equal(typeof tau.noConflict, 'function', 'Class tau.noConflict exists');
+	});
+}(window.tau));
