37d0bbe9082fe66f9f8ca16e2e3c7bfcf93f64fc
heeju Joo <heeju.joo@samsung.com>
2014-08-13 18:42:17 +0900

    MobileWinset: rearranged list of widget pages
    Change-Id: I0e8a16ac9dcbe32c3aa3e77acf2b7f82df7093f9

--- tau/demos/MobileWinset/src/tips/two-line-text/two-line-text.js
+++
@@ -1,8 +0,0 @@
-$( document ).one( "pagebeforeshow", "#twolinesample", function ( e ) {
-	$( "#textposition .ui-btn-text" ).css( "font-size", "12px" );
-	$( "#textposition" ).find( "span" ).css( {
-		"line-height": "10px" ,
-		"padding-top": "2px",
-		"padding-bottom": "2px"
-	});
-});
