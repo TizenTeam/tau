87d5820d059fb2b9dc9fd5384f9cb6c37d3973ad
Hyunkook, Cho <hk0713.cho@samsung.com>
2014-09-22 11:10:38 +0900

    [jira] fixed page tab navigation.
    Added data-auto-change option.
    Added page ID for page navigation.
    Modified main.js for back key control.
    
    Change-Id: If16e41f61ddcf55d0a36788be30c778574523fdc

--- tau/demos/SDK/Tizen_Web_UI_FW_NavigationView/js/main.js
+++ tau/demos/SDK/Tizen_Web_UI_FW_NavigationView/js/main.js
@@ -1,45 +1,16 @@
-var backEventListener = null;
-
-var unregister = function() {
-    if ( backEventListener !== null ) {
-        document.removeEventListener( 'tizenhwkey', backEventListener );
-        backEventListener = null;
-        window.tizen.application.getCurrentApplication().exit();
-    }
-}
-
-//Initialize function
-var init = function () {
-    // register once
-    if ( backEventListener !== null ) {
-        return;
-    }
-    
-    // TODO:: Do your initialization job
-    console.log("init() called");
-    
-    var backEvent = function(e) {
-        if ( e.keyName == "back" ) {
-            try {
-                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
-                    // if first page, terminate app
-                    unregister();
-                } else {
-                    // move previous page
-                    $.mobile.urlHistory.activeIndex -= 1;
-                    $.mobile.urlHistory.clearForward();
-                    window.history.back();
-                }
-            } catch( ex ) {
-                unregister();
-            }
-        }
-    }
-    
-    // add eventListener for tizenhwkey (Back Button)
-    document.addEventListener( 'tizenhwkey', backEvent );
-    backEventListener = backEvent;
-};
-
-$(document).bind( 'pageinit', init );
-$(document).unload( unregister );
+( function () {
+	window.addEventListener( 'tizenhwkey', function( ev ) {
+		if( ev.keyName == "back" ) {
+			var page = document.getElementsByClassName( 'ui-page-active' )[0],
+				pageid = page ? page.id : "";
+			if( pageid === "main" ) {
+				try {
+					tizen.application.getCurrentApplication().exit();
+				} catch (ignore) {
+				}
+			} else {
+				window.history.back();
+			}
+		}
+	} );
+} () );
