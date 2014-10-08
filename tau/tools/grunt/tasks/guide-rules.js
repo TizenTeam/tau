a5bc00362d894388088dcf2a57a44d6adff46ee5
Piotr Kusztal <p.kusztal@samsung.com>
2014-10-08 15:32:12 +0200

    [DOCS] Developer Guide - Framework Profiles
    Change-Id: I3b00dadf5e58842e1cfc929d2d439237345a87c4
    Signed-off-by: Michał Szepielak <m.szepielak@samsung.com>
    Signed-off-by: Piotr Kusztal <p.kusztal@samsung.com>

--- tau/tools/grunt/tasks/guide-rules.js
+++ tau/tools/grunt/tasks/guide-rules.js
@@ -1,4 +1,4 @@
 /*global module */
 module.exports = {
-	baloon: /^(!(warning|info|danger)\n([^\n]+)\n([^\n]+)\n*)+/gim
+	baloon: /^(!(warning|info|danger)\n?([^\n]*?)\n([^\n]+([\w _,.]+[\n])+))/gm
 };
