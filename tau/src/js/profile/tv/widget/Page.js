b004b790200d0a9e027c57e09924bacde5453289
Piotr Czajka <p.czajka@samsung.com>
2014-10-16 09:45:50 +0200

    [TV] Added documentation for Page widget.
    Change-Id: I7d668a6d2cde003f90f79f7d7dad5d471e8d93e2
    Signed-off-by: Piotr Czajka <p.czajka@samsung.com>

--- tau/src/js/profile/tv/widget/Page.js
+++ tau/src/js/profile/tv/widget/Page.js
@@ -6,7 +6,72 @@
 /*jslint nomen: true, plusplus: true */
 /**
  * # Page Widget
- * Page is the main element of application's structure.
+ * Page is main element of application's structure.
+ *
+ * ## Default selectors
+ * In the Tizen TV Web UI framework the application page structure is based on a header, content and footer elements:
+ *
+ * - **The header** is placed at the top, and displays the page title and optionally buttons.
+ * - **The content** is the section below the header, showing the main content of the page.
+ * - **The footer** is abottom part of page which can display for example buttons
+ *
+ * ## Examples
+ *
+ * ### Empty header & footer
+ * This is the standard page which can be fully arranged as one wishes. It is the basic version for all
+ * pages.
+ * 	@example
+ * 		<div id="normal-page" class="ui-page" data-size="mini">
+ * 			<div class="ui-content content-padding">
+ * 				For create normal page create tag with class <i>ui-page</i>
+ * 			</div>
+ * 		</div>
+ *
+ * ### Title in header
+ * A standard page can me enchanced with header containing title.
+ * 	@example
+ * 		<div id="normal-page" class="ui-page" data-size="mini">
+ * 			<header class="ui-header">
+ * 				<h2 class="ui-title">Title in header</h2>
+ * 			</header>
+ * 			<div class="ui-content content-padding">
+ * 				For create normal page create tag with class <i>ui-page</i>
+ * 			</div>
+ * 		</div>
+ *
+ * ### Buttons in header
+ * You can add buttons to header in case of need. By default text will appear on the left
+ * side of the header and buttons on the right side.
+ * 	@example
+ * 		<div id="normal-page" class="ui-page" data-size="mini">
+ * 			<header class="ui-header">
+ * 				<a href="#" data-role="button">button 1</a>
+ * 				<a href="#" data-role="button">button 2</a>
+ * 				<h2 class="ui-title">Title in header</h2>
+ * 			</header>
+ * 			<div class="ui-content content-padding">
+ * 				For create normal page create tag with class <i>ui-page</i>
+ * 			</div>
+ * 		</div>
+ *
+ * ### Structure with header and footer tag
+ * 	@example
+ * 		<div id="normal-page" class="ui-page" data-size="mini">
+ * 			<header class="ui-header">
+ * 				<h2 class="ui-title">Title in header</h2>
+ * 			</header>
+ * 			<div class="ui-content content-padding"></div>
+ * 			<footer>
+ * 				<div data-role="controlgroup" data-type="horizontal">
+ * 					<a href="#" data-role="button">Button in footer</a>
+ * 				</div>
+ * 			</footer>
+ * 		</div>
+ *
+ * ## Focus
+ * Focus on page is working straightforward. Using diectional keys will make focus move in the
+ * respective direction. Also using OK[remote control]/Enter[keyboard] while widget is focused
+ * will send activation signal to it (response will differ between widgets).
  *
  * @class ns.widget.tv.Page
  * @extends ns.widget.wearable.Page
@@ -27,19 +92,75 @@
 		],
 		function () {
 			//>>excludeEnd("tauBuildExclude");
+			/**
+			 * Alias for {@link ns.widget.wearable.Page}
+			 * @property {Object} Page
+			 * @member ns.widget.tv.Page
+			 * @private
+			 * @static
+			 */
 			var WearablePage = ns.widget.wearable.Page,
 				WearablePagePrototype = WearablePage.prototype,
+				/**
+				 * Alias for {@link ns.widget.tv.BaseKeyboardSupport}
+				 * @property {Object} BaseKeyboardSupport
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				BaseKeyboardSupport = ns.widget.tv.BaseKeyboardSupport,
+				/**
+				 * Dictionary for page related css class names.
+				 * It bases on {@link ns.widget.wearable.Page.classes}
+				 * @property {Object} classes
+				 * @member ns.widget.tv.Page
+				 * @static
+				 */
 				classes = WearablePage.classes,
+				/**
+				 * Alias for {@link ns.wearable.selectors}
+				 * @property {Object} selectors
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				selectors = ns.wearable.selectors,
+				/**
+				 * Alias for {@link ns.util}
+				 * @property {Object} util
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				util = ns.util,
+				/**
+				 * Alias for {@link ns.util.DOM}
+				 * @property {Object} DOM
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				DOM = util.DOM,
+				/**
+				 * Alias for {@link ns.util.selectors}
+				 * @property {Object} utilSelectors
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				utilSelectors = util.selectors,
 				Page = function () {
 					var self = this;
 					BaseKeyboardSupport.call(self);
 					self._ui = self._ui || {};
 				},
+				/**
+				 * Alias for {@link ns.engine}
+				 * @property {Object} engine
+				 * @member ns.widget.tv.Page
+				 * @private
+				 * @static
+				 */
 				engine = ns.engine,
 				FUNCTION_TYPE = "function",
 				prototype = new WearablePage();
@@ -51,16 +172,33 @@
 			Page.events = WearablePage.events;
 			Page.classes = classes;
 
+			/**
+			 * This method triggers SHOW event and turns keyboard support on.
+			 * @method onShow
+			 * @member ns.widget.tv.Page
+			 */
 			prototype.onShow = function() {
 				WearablePagePrototype.onShow.call(this);
 				this._supportKeyboard = true;
 			};
 
+			/**
+			 * This method triggers HIDE event and turns keyboard support on.
+			 * @method onHide
+			 * @member ns.widget.tv.Page
+			 */
 			prototype.onHide = function() {
 				WearablePagePrototype.onHide.call(this);
 				this._supportKeyboard = false;
 			};
 
+			/**
+			 * Method creates buttons defined in page header.
+			 * @method _buildButtonsInHeader
+			 * @param {HTMLElement} header
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._buildButtonsInHeader = function (header) {
 				var headerButtons = [].slice.call(header.querySelectorAll("[data-role='button']")),
 					headerButtonsLength = headerButtons.length,
@@ -70,6 +208,13 @@
 				}
 			};
 
+			/**
+			 * Method creates title defined in page header.
+			 * @method _buildTitleInHeader
+			 * @param {HTMLElement} header
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._buildTitleInHeader = function(header) {
 				var title = header.querySelector("h1,h2,h3,h4,h5,h6,h7,h8");
 				if (title) {
@@ -77,6 +222,14 @@
 				}
 			};
 
+			/**
+			 * Method creates empty page header. It also checks for additional
+			 * content to be added in header.
+			 * @method _buildHeader
+			 * @param {HTMLElement} element
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._buildHeader = function(element) {
 				var self = this,
 					header = utilSelectors.getChildrenBySelector(element, "header,." + classes.uiHeader)[0];
@@ -91,6 +244,13 @@
 				self._ui.header = header;
 			};
 
+			/**
+			 * Method creates empty page footer.
+			 * @method _buildFooter
+			 * @param {HTMLElement} element
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._buildFooter = function(element) {
 				var footer = utilSelectors.getChildrenBySelector(element, "footer,." + classes.uiFooter)[0];
 				// add class if footer does not exist
@@ -102,6 +262,13 @@
 				this._ui.footer = footer;
 			};
 
+			/**
+			 * Method creates empty page footer.
+			 * @method _buildContent
+			 * @param {HTMLElement} element
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._buildContent = function(element) {
 				var content = utilSelectors.getChildrenByClass(element, classes.uiContent)[0],
 					next,
@@ -123,6 +290,14 @@
 				ui.content = content;
 			};
 
+			/**
+			 * Build page
+			 * @method _build
+			 * @param {HTMLElement} element
+			 * @return {HTMLElement}
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._build = function(element) {
 				var self = this;
 
@@ -137,6 +312,13 @@
 				return element;
 			};
 
+			/**
+			 * Clears header and footer class lists.
+			 * @method _clearHeaderFooterInfo
+			 * @param {HTMLElement} element
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._clearHeaderFooterInfo = function(element) {
 				var elementClassList = element.classList;
 				elementClassList.remove(classes.uiHeaderEmpty);
@@ -147,7 +329,8 @@
 			 * Sets top-bottom css attributes for content element
 			 * to allow it to fill the page dynamically
 			 * @method _contentFill
-			 * @member ns.widget.wearable.Page
+			 * @protected
+			 * @member ns.widget.tv.Page
 			 */
 			prototype._contentFill = function () {
 				var self = this,
@@ -198,16 +381,35 @@
 				}
 			};
 
+			/**
+			 * Bind events to widget
+			 * @method _bindEvents
+			 * @param {HTMLElement} element
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._bindEvents = function(element) {
 				WearablePagePrototype._bindEvents.call(this, element);
 				this._bindEventKey();
 			};
 
+			/**
+			 * Destroy widget
+			 * @method _destroy
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._destroy = function() {
 				this._destroyEventKey();
 				WearablePagePrototype._destroy.call(this);
 			};
 
+			/**
+			 * Refresh widget structure
+			 * @method _refresh
+			 * @protected
+			 * @member ns.widget.tv.Page
+			 */
 			prototype._refresh = function() {
 				var self = this,
 					element = self.element;
