261f3cfaf72535b9ba77e6ad42e2a5fbc6e6f456
Sergiusz Struminski <s.struminski@samsung.com>
2014-12-02 16:34:02 +0100

    etc(tool): ThemeEditor run script with new profile, display help for cli
    Change-Id: Ib220ada9f57838abf05c05b8b5cd571a0fbcbe6a
    Signed-off-by: Sergiusz Struminski <s.struminski@samsung.com>

--- tools/ThemeEditor/src/run.sh
+++ tools/ThemeEditor/src/run.sh
@@ -1,9 +1,18 @@
+function displayHelp {
+	echo "You can use following options:"
+	echo "./run.sh 'name=DisplayedAppName&path=pathToApp'"
+}
+
 CUR=$(cd $(dirname $0) && pwd)
 CHROME=google-chrome
+NOW=`date +%s`
+TMPDIR="/tmp/.ThemeEditor"
+USER_DATA_DIR="$TMPDIR/$NOW"
 
 if test -z "$1"
 then
 	START_CONFIG=""
+	displayHelp
 else
 	START_CONFIG="?"$1
 fi
@@ -17,4 +26,8 @@ if [ "$OS" == "darwin" ] || [ "$(uname)" == "Darwin" ]; then
 	fi
 fi
 
-"$CHROME" --allow-file-access-from-files --disable-web-security --start-maximized --app="file://$CUR/index.html$START_CONFIG"
+rm -rf $TMPDIR
+mkdir $TMPDIR
+mkdir $USER_DATA_DIR
+
+"$CHROME" --user-data-dir=$USER_DATA_DIR --no-first-run --no-default-browser-check --allow-file-access-from-files --disable-web-security --start-maximized --app="file://$CUR/index.html$START_CONFIG"
