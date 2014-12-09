e358eb3d8fff2aea69cd06ca997329151c7bf9c6
heeju Joo <heeju.joo@samsung.com>
2014-12-09 20:18:59 +0900

    feat(core): Add Marquee widget
    [widget description]
    - marquee widget
    - makes div marquee-able using pre-defined webkit animation
    - marquee style: slide(default), scroll, alternate
    [additional feature]
    - With long text, it add gradient effect in the end of marquee object
    - it is option of marquee (ellipsisEffect: "gradient", "ellipsis", "none")
    [usage example]
    - in HTML code: <div class="ui-marquee" id="marquee">Long text for marquee</div>
    - in JS code:
    marquee = document.getElementById("marquee");
    marqueeWidget = new tau.widget.Marquee(marquee, {delay: "3", timingFunction: "ease-in"});
    - opened API: start(), stop(), reset()
    API-Change: core/marquee
    Change-Id: Ic2d9a7c42be163561414a8090f30423b22829ed1
    Signed-off-by: heeju Joo <heeju.joo@samsung.com>

---
+++ tau/demos/SDK/WearableCircleWidgetSample/extra/marquee/marquee.js
@@ -0,0 +1,79 @@
+(function() {
+	var page = document.getElementById("marqueeTest"),
+		marqueeEl = document.getElementById("marquee"),
+		startBtn = document.getElementById("start"),
+		resetBtn = document.getElementById("reset"),
+		startFlag = false,
+		marqueeWidget;
+
+	function unbindEvents () {
+		page.removeEventListener("pageshow", pageShowHandler);
+		page.removeEventListener("pagehide", pageHideHandler);
+		startBtn.removeEventListener("vclick", marqueeStartandStop);
+		resetBtn.removeEventListener("vclick", marqueeReset);
+		marqueeEl.removeEventListener("marqueeend", marqueeEndHandler);
+		marqueeEl.removeEventListener("marqueestart", marqueeStartHandler);
+		marqueeEl.removeEventListener("marqueestopped", marqueeStoppedHandler);
+	};
+
+	function clearVariables () {
+		page = null;
+		marqueeEl = null;
+		startBtn = null;
+		resetBtn = null;
+		marqueeWidget = null;
+	};
+
+	function marqueeEndHandler () {
+		startFlag = false;
+		console.log("marquee end!");
+	};
+
+	function marqueeStartHandler () {
+		startFlag = true;
+		console.log("marquee Start!");
+	};
+
+	function marqueeStoppedHandler () {
+		startFlag = false;
+		console.log("marquee Stopped!");
+	};
+
+	function marqueeStartandStop () {
+		if (startFlag) {
+			startBtn.textContent = "START";
+			marqueeWidget.stop();
+		} else {
+			startBtn.textContent = "STOP";
+			marqueeWidget.start();
+		}
+	};
+
+	function marqueeReset () {
+		startBtn.textContent = "START";
+		marqueeWidget.reset();
+	};
+
+	function bindEvents () {
+		marqueeEl.addEventListener("marqueeend", marqueeEndHandler);
+		marqueeEl.addEventListener("marqueestart", marqueeStartHandler);
+		marqueeEl.addEventListener("marqueestopped", marqueeStoppedHandler);
+		startBtn.addEventListener("vclick", marqueeStartandStop);
+		resetBtn.addEventListener("vclick", marqueeReset);
+	};
+
+	function pageShowHandler () {
+		bindEvents();
+		marqueeWidget = new tau.widget.Marquee(marqueeEl, {autoRun:false});
+	};
+
+	function pageHideHandler () {
+		marqueeWidget.destroy();
+		unbindEvents();
+		clearVariables();
+	};
+
+	page.addEventListener("pageshow", pageShowHandler, false);
+	page.addEventListener("pagehide", pageHideHandler, false);
+}());
+
