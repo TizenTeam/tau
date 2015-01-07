07c0c7445a823c27fbab67bf946841f10b62cde9
junhyeonLee <juneh.lee@samsung.com>
2015-01-07 10:32:31 +0900

    etc(wearable): delete previous sample
    Delete previously made sample app
    to adapy new Sample app.
    
    Change-Id: I1017a1032942c64f3b88e882a47f52fc9018bd72
    Signed-off-by: junhyeonLee <juneh.lee@samsung.com>

--- tau/demos/SDK/WearableCircleWidgetSample/basic/toggle/toggle_full.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var toggleSwitch = document.getElementById("toggle-full"),
-		subText = document.getElementsByClassName("ui-switch-sub-text")[0];
-
-	toggleSwitch.addEventListener("change", function(){
-		subText.innerText =
-			toggleSwitch.checked === true ? "On" : "Off";
-	});
-})();
