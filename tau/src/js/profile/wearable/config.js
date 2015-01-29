9de572a9009f3260f4d616b99446c1a2b48a03c5
hagun.kim <hagun.kim@samsung.com>
2015-01-29 15:43:15 +0900

    refactor(wearable): remove ui-scroll-on class in sample page
    1. EnablePageScroll option is added as global.
    2. Global option for circle theme will be applied
    depending on media query.
    3. All of ui-scroll-on class with page is removed.
    4. Since patch set #2, default.js is deleted in demo.
    Global option is set in config.js in wearable profile.
    
    Change-Id: Ic19c56acd340c901862397fa5ff9dfde6f966666
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

--- tau/src/js/profile/wearable/config.js
+++ tau/src/js/profile/wearable/config.js
@@ -15,11 +15,19 @@
 		],
 		function () {
 			//>>excludeEnd("tauBuildExclude");
-
+			var mql = window.matchMedia("(-tizen-geometric-shape: circle)");
 			// Default configuration properties for wearable
-			ns.setConfig('autoBuildOnPageChange', false, true);
-			ns.setConfig('popupTransition', 'slideup');
-			ns.setConfig('enablePageScroll', false);
+			ns.setConfig("autoBuildOnPageChange", false, true);
+			if(mql.matches) {
+				ns.setConfig("pageTransition", "pop");
+				ns.setConfig("popupTransition", "pop");
+				ns.setConfig("popupFullSize", true);
+				ns.setConfig("scrollEndEffectArea", "screen");
+				ns.setConfig("enablePageScroll", true);
+			} else {
+				ns.setConfig("popupTransition", "slideup");
+				ns.setConfig("enablePageScroll", false);
+			}
 			// .. other possible options
 			// ns.setConfig('autoInitializePage', true);
 			// ns.setConfig('pageContainer', document.body); // defining application container for wearable
