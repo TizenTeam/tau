cbf781027d9dd1def836fe0a4fa7fe1658b9a140
hagun.kim <hagun.kim@samsung.com>
2015-02-23 13:32:37 +0900

    refactor(wearable): add integrated samples
    Footer button, More options, Thumbnail samples are added
    from integration directory to CircularUIComponents directory.
    
    Change-Id: I88a48f447fbee4e6761ca0ce8b02f4b9172b6721
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

---
+++ tau/demos/SDK/wearable/circular/CircularUIComponents/thumbnail/thumbnail.js
@@ -0,0 +1,11 @@
+(function(){
+	var page = document.getElementById("sectionChangerPage"),
+		sectionChanger = document.getElementById("sectionChanger");
+
+	page.addEventListener( "pagebeforeshow", function() {
+		tau.widget.SectionChanger(sectionChanger, {
+			orientation: "horizontal",
+			fillContent: false
+		});
+	});
+})();
