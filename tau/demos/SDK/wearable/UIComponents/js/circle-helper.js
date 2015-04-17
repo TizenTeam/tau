d7b2327141b080443c2e350dda806271baf1db2b
hagun.kim <hagun.kim@samsung.com>
2015-04-17 21:02:28 +0900

    refactor(wearable): remove list basic animation and add sample separately.
    1. list animation is removed.
    2. list animation sample is added separately
    3. divider list active bg color in round UI is fixed.
    
    Change-Id: Ib21e9c5e2d40a1d38c8405f8b7853630fa4100fb
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/js/circle-helper.js
+++ tau/demos/SDK/wearable/UIComponents/js/circle-helper.js
@@ -1,12 +1,10 @@
 (function(tau) {
 	var page,
 		pageWidget,
-		enablePageScroll,
 		elScroller,
 		list,
 		listHelper = [],
 		snapList = [],
-		header,
 		headerExpandHandler = [],
 		headerCollapseHandler = [],
 		i, len;
@@ -16,49 +14,42 @@
 			page = e.target;
 
 			pageWidget = tau.widget.page(page);
-			enablePageScroll = pageWidget.option("enablePageScroll");
 			elScroller = page.querySelector(".ui-scroller");
-			header = page.querySelector(".ui-header:not(.ui-fixed)");
-
 			if (elScroller) {
 				list = elScroller.querySelectorAll(".ui-listview");
-				if (page.id !== "pageMarqueeList" && page.id !== "pageTestVirtualList") {
-					len = list.length;
-					for (i = 0; i < len; i++) {
-						listHelper[i] = tau.helper.SnapListStyle.create(list[i], {animate: "scale"});
+				if (list) {
+					if (page.id !== "pageMarqueeList" && page.id !== "pageTestVirtualList" && page.id !== "pageAnimation") {
+						len = list.length;
+						for (i = 0; i < len; i++) {
+							listHelper[i] = tau.helper.SnapListStyle.create(list[i]);
+						}
+						len = listHelper.length;
+						if (len) {
+							for (i = 0; i < len; i++) {
+								snapList[i] = listHelper[i].getSnapList();
+								headerCollapseHandler[i] = snapList[i].enable.bind(snapList[i]);
+								headerExpandHandler[i] = snapList[i].disable.bind(snapList[i]);
+								page.addEventListener("headercollapse", headerCollapseHandler[i], false);
+								page.addEventListener("headerbeforeexpand", headerExpandHandler[i], false);
+							}
+						}
 					}
-				}
-				elScroller.setAttribute("tizen-circular-scrollbar", "");
-			}
-
-			if (header && enablePageScroll) {
-				len = listHelper.length;
-
-				if (len) {
-					for (i = 0; i < len; i++) {
-						snapList[i] = listHelper[i].getSnapList();
-						headerCollapseHandler[i] = snapList[i].enable.bind(snapList[i]);
-						headerExpandHandler[i] = snapList[i].disable.bind(snapList[i]);
-						header.addEventListener("headercollapse", headerCollapseHandler[i], false);
-						header.addEventListener("headerbeforeexpand", headerExpandHandler[i], false);
-					}
+					elScroller.setAttribute("tizen-circular-scrollbar", "");
 				}
 			}
 		});
 
 		document.addEventListener("pagebeforehide", function (e) {
 			len = listHelper.length;
-			if (elScroller) {
-				if (len) {
-					for (i = 0; i < len; i++) {
-						listHelper[i].destroy();
-						if (header) {
-							header.removeEventListener("headercollapse", headerCollapseHandler[i], false);
-							header.removeEventListener("headerbeforeexpand", headerExpandHandler[i], false);
-						}
-					}
-					listHelper = [];
+			if (len) {
+				for (i = 0; i < len; i++) {
+					listHelper[i].destroy();
+					page.removeEventListener("headercollapse", headerCollapseHandler[i], false);
+					page.removeEventListener("headerbeforeexpand", headerExpandHandler[i], false);
 				}
+				listHelper = [];
+			}
+			if(elScroller) {
 				elScroller.removeAttribute("tizen-circular-scrollbar");
 			}
 		});
