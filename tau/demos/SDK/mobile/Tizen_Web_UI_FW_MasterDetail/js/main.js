95fd5f244f3e91089eb88d8bb49e85e757411ff9
heeju Joo <heeju.joo@samsung.com>
2015-04-20 10:46:15 +0900

    fix(mobile): modify page exit statement in sample
    - when it has active popup in main page, user press back, app is exit.
    - so in app.js, script checks first if there is active popup.
    
    [Issue-URL: http://168.219.209.56/jira/browse/TMWC-752]
    Change-Id: I09c3a9969f4b06b3c22b956b92df2c696a0e8296
    Signed-off-by: heeju Joo <heeju.joo@samsung.com>

--- tau/demos/SDK/mobile/Tizen_Web_UI_FW_MasterDetail/js/main.js
+++ tau/demos/SDK/mobile/Tizen_Web_UI_FW_MasterDetail/js/main.js
@@ -1,9 +1,11 @@
 ( function () {
     window.addEventListener( 'tizenhwkey', function( ev ) {
         if( ev.keyName === "back" ) {
-            var page = document.getElementsByClassName( 'ui-page-active' )[0],
+            var activePopup = document.querySelector( '.ui-popup-active' ),
+                page = document.getElementsByClassName( 'ui-page-active' )[0],
                 pageid = page ? page.id : "";
-            if( pageid === "main" ) {
+
+            if( pageid === "main" && !activePopup ) {
                 try {
                     tizen.application.getCurrentApplication().exit();
                 } catch (ignore) {
--- tau/demos/SDK/mobile/Tizen_Web_UI_FW_SinglePage/js/main.js
+++ tau/demos/SDK/mobile/Tizen_Web_UI_FW_SinglePage/js/main.js
@@ -1,9 +1,11 @@
 ( function () {
     window.addEventListener( 'tizenhwkey', function( ev ) {
-        if( ev.keyName == "back" ) {
-            var page = document.getElementsByClassName( 'ui-page-active' )[0],
+        if( ev.keyName === "back" ) {
+            var activePopup = document.querySelector( '.ui-popup-active' ),
+                page = document.getElementsByClassName( 'ui-page-active' )[0],
                 pageid = page ? page.id : "";
-            if( pageid === "main" ) {
+
+            if( pageid === "main" && !activePopup ) {
                 try {
                     tizen.application.getCurrentApplication().exit();
                 } catch (ignore) {
