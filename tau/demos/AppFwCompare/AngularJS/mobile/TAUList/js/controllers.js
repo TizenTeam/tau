c76a2d75faaa128834713125398f81b47c8ecc7c
Piotr Karny <p.karny@samsung.com>
2015-04-21 10:00:25 +0200

    demos(all): AngularJS example apps
    Change-Id: I30eb07c7e69aff90d704a5083f18d02045e2e418
    Signed-off-by: Piotr Karny <p.karny@samsung.com>

---
+++ tau/demos/AppFwCompare/AngularJS/mobile/TAUList/js/controllers.js
@@ -0,0 +1,23 @@
+var helloApp = angular.module('helloApp', []);
+
+helloApp.controller('HelloController', function ($scope) {
+	$scope.players = null;
+	//$scope.players = JSON_DATA;
+	$scope.loadPlayers = function () {
+		console.time("templ-a");
+		console.time("templ-tau");
+		$scope.players = JSON_DATA;
+	};
+
+	$scope.$watch("players", function (oldValue, newValue) {
+		var list = tau.widget.getInstance(document.getElementById("listviewtest"), "Listview");
+
+		if (list) {
+			setTimeout(function () {
+				console.timeEnd("templ-a");
+				list.refresh();
+				console.timeEnd("templ-tau");
+			}, 0);
+		}
+	});
+});
