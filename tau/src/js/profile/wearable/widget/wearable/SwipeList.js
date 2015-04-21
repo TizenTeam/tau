ce0f14cd0e95b1e5b15c151133a9af5c9738e688
Hyeoncheol Choi <hc7.choi@samsung.com>
2015-04-21 15:14:48 +0900

    fix(wearable): fix swipelist position issue
    swipelist position algorithm fixed
    
    Change-Id: I237f995477dec31c16160e03043fa0c7889d7914
    Signed-off-by: Hyeoncheol Choi <hc7.choi@samsung.com>

--- tau/src/js/profile/wearable/widget/wearable/SwipeList.js
+++ tau/src/js/profile/wearable/widget/wearable/SwipeList.js
@@ -231,8 +231,10 @@
 					this.container = element.parentNode;
 				}
 
-				this.container.style.position = "relative";
-
+				this.scrollableElement = selectors.getScrollableParent(element);
+				if (!this.scrollableElement) {
+					this.scrollableElement = this.container;
+				}
 				this.swipeElement = page.querySelector(options.swipeElement);
 				this.swipeLeftElement = options.swipeLeftElement ? page.querySelector(options.swipeLeftElement) : undefined;
 				this.swipeRightElement = options.swipeRightElement ? page.querySelector(options.swipeRightElement) : undefined;
@@ -241,8 +243,8 @@
 					this.swipeElementStyle = this.swipeElement.style;
 					this.swipeElementStyle.display = "none";
 					this.swipeElementStyle.background = "transparent";
-					this.swipeElementStyle.width = this.container.offsetWidth + "px";
-					this.swipeElementStyle.height = this.container.offsetHeight + "px";
+					this.swipeElementStyle.width = this.scrollableElement.offsetWidth + "px";
+					this.swipeElementStyle.height = this.scrollableElement.offsetHeight + "px";
 				}
 
 				if (this.swipeLeftElement) {
@@ -405,7 +407,7 @@
 
 			prototype._start = function (e) {
 				var gesture = e.detail,
-					containerTop, width, height, top;
+					width, height, top;
 
 				this._dragging = false;
 				this._cancelled = false;
@@ -416,10 +418,7 @@
 
 					width = this.activeTarget.offsetWidth;
 					height = this.activeTarget.offsetHeight;
-					containerTop = this.container.scrollTop;
-					top = this.activeTarget.offsetTop - containerTop;
-
-					this.swipeElementStyle.top = containerTop + "px";
+					top = this.activeTarget.offsetTop - this.scrollableElement.scrollTop;
 
 					if (this.swipeLeftElementStyle) {
 						this.swipeLeftElementStyle.width = width + "px";
