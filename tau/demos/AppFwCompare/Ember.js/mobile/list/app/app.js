e8a84c5c28c612db4c76433eff61054a0c465fa3
Krzysztof Antoszek <k.antoszek@samsung.com>
2015-04-21 09:42:09 +0200

    demo(all): Ember.js demo applications
    Change-Id: I4f65a63f30b0d925610dec24769febf893ac7361
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

---
+++ tau/demos/AppFwCompare/Ember.js/mobile/list/app/app.js
@@ -0,0 +1,51 @@
+/*global window */
+/*jslint browser: true, nomen: true */
+(function (window, Ember, DS, dom, data) {
+	"use strict";
+	var App = Ember.Application.create();
+	App.Store = DS.Store.extend({});
+
+	App.IndexView = Ember.View.extend({
+		classNames: "ui-page"
+	});
+
+	App.ListItemView = Ember.View.extend({
+		tagName: "",
+		templateName: "listItem"
+	});
+
+	App.ListViewView = Ember.CollectionView.extend({
+		content: data,
+		tagName: "",
+		itemViewClass: App.ListItemView,
+		didInsertElement: function () {
+			this._super();
+			Ember.run.scheduleOnce("afterRender", this, function () {
+				tau.engine.instanceWidget(document.getElementById("listviewtest"), "Listview").refresh();				
+				console.timeEnd("listview");
+			});
+		}
+	});
+
+	App.IndexRoute = Ember.Route.extend({
+		setupController: function (controller, model) {
+			this._super(controller, model);
+			controller.set("title", "NBA Players");
+		},
+		actions: {
+			doIt: function () {
+				console.time("listview");
+				this.send("renderTitle");
+				this.send("renderListview");
+			},
+
+			renderListview: function () {
+				this.render("list", {into: "index", outlet: "listviewHolder"});
+			},
+
+			renderTitle: function () {
+				this.render("indexTitle", {into: "index", outlet: "titleHolder"});
+			}
+		}
+	});
+}(window, window.Ember, window.DS, window.document, window.JSON_DATA));
