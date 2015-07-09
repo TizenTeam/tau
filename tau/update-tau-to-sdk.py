a0cc3803e54d226a53d38e6c624c028008a24a7b
heeju Joo <heeju.joo@samsung.com>
2015-07-09 16:31:09 +0900

    refactor(etc): update release script to tizen_2.4
    Change-Id: I0b34a697c72f98e9043de7f1ab7a00b0baca2cd5
    Signed-off-by: heeju Joo <heeju.joo@samsung.com>

--- tau/update-tau-to-sdk.py
+++ tau/update-tau-to-sdk.py
@@ -43,60 +43,67 @@ class Job:
 # Git info
 class webuifw(Git):
 	addr="168.219.209.56:29418/framework/web/web-ui-fw"
-	branch="devel/tizen_2.3.1"
+	branch="devel/tizen_2.4"
 
 class MobileUIComponent(Git):
 	addr="168.219.209.56:29418/apps/mobile/web/sample/tizen-winset"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
+
+class TizenGlobalize(Git):
+	addr="168.219.209.56:29418/apps/mobile/web/sample/tizen-globalize"
+	branch="tizen_2.4"
 
 class TAUMasterDetail(Git):
 	addr="168.219.209.56:29418/apps/mobile/web/template/tau-master-detail"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class TAUMultiPage(Git):
 	addr="168.219.209.56:29418/apps/mobile/web/template/tau-multi-page"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class TAUNavigationView(Git):
 	addr="168.219.209.56:29418/apps/mobile/web/template/tau-navigation-view"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class TAUSinglePage(Git):
 	addr="168.219.209.56:29418/apps/mobile/web/template/tau-single-page"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class WearableUIComponent(Git):
 	addr="168.219.209.56:29418/apps/wearable/web/sample/wearable-widget-sample"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class TAUBasic(Git):
 	addr="168.219.209.56:29418/apps/wearable/web/template/tau-basic"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 class TAUList(Git):
 	addr="168.219.209.56:29418/apps/wearable/web/template/tau-list"
-	branch="tizen_2.3"
+	branch="tizen_2.4"
 
 # job list
 jobs = {
 	"mobile": Job(
 		webuifw,
-		[MobileUIComponent, TAUMasterDetail, TAUMultiPage, TAUNavigationView, TAUSinglePage],
+		[MobileUIComponent, TizenGlobalize, TAUMasterDetail, TAUMultiPage, TAUNavigationView, TAUSinglePage],
 		[
 			SrcDest("web-ui-fw/tau/demos/SDK/mobile/UIComponents", "tizen-winset/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tizen-winset/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/mobile/Tizen_Web_UI_FW_MasterDetail", "tau-master-detail/project"),
+			SrcDest("web-ui-fw/tau/dist/mobile", "tizen-globalize/project/lib/tau"),
+			SrcDest("web-ui-fw/tau/demos/SDK/mobile/MasterDetail", "tau-master-detail/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-master-detail/project/lib/tau"),
-            SrcDest("web-ui-fw/tau/demos/SDK/mobile/Tizen_Web_UI_FW_MultiPage", "tau-multi-page/project"),
-            SrcDest("web-ui-fw/tau/dist/mobile", "tau-multi-page/project/lib/tau"),
-            SrcDest("web-ui-fw/tau/demos/SDK/mobile/Tizen_Web_UI_FW_NavigationView", "tau-navigation-view/project"),
+            		SrcDest("web-ui-fw/tau/demos/SDK/mobile/MultiPage", "tau-multi-page/project"),
+            		SrcDest("web-ui-fw/tau/dist/mobile", "tau-multi-page/project/lib/tau"),
+            		SrcDest("web-ui-fw/tau/demos/SDK/mobile/NavigationView", "tau-navigation-view/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-navigation-view/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/mobile/Tizen_Web_UI_FW_SinglePage", "tau-single-page/project"),
+			SrcDest("web-ui-fw/tau/demos/SDK/mobile/SinglePage", "tau-single-page/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-single-page/project/lib/tau")
-		], ["cd web-ui-fw/tau", "grunt build"],
+		], ["cd web-ui-fw/tau", "npm install", "grunt build"],
 		[
 			SrcDest("web-ui-fw/tau/dist/VERSION", "tizen-winset/project/lib/tau/VERSION"),
 			SrcDest("web-ui-fw/tau/dist/LICENSE.MIT", "tizen-winset/project/lib/tau/LICENSE.MIT"),
+			SrcDest("web-ui-fw/tau/dist/VERSION", "tizen-globalize/project/lib/tau/VERSION"),
+			SrcDest("web-ui-fw/tau/dist/LICENSE.MIT", "tizen-globalize/project/lib/tau/LICENSE.MIT"),
 			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-master-detail/project/lib/tau/VERSION"),
 			SrcDest("web-ui-fw/tau/dist/LICENSE.MIT", "tau-master-detail/project/lib/tau/LICENSE.MIT"),
 			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-multi-page/project/lib/tau/VERSION"),
@@ -112,11 +119,11 @@ jobs = {
     		[
     			SrcDest("web-ui-fw/tau/demos/SDK/wearable/UIComponents", "wearable-widget-sample/project"),
     			SrcDest("web-ui-fw/tau/dist/wearable", "wearable-widget-sample/project/lib/tau"),
-    			SrcDest("web-ui-fw/tau/demos/SDK/wearable/TemplateBasic", "tau-basic/project"),
+    			SrcDest("web-ui-fw/tau/demos/SDK/wearable/Basic", "tau-basic/project"),
     			SrcDest("web-ui-fw/tau/dist/wearable", "tau-basic/project/lib/tau"),
-    			SrcDest("web-ui-fw/tau/demos/SDK/wearable/TemplateList", "tau-list/project"),
+    			SrcDest("web-ui-fw/tau/demos/SDK/wearable/List", "tau-list/project"),
     			SrcDest("web-ui-fw/tau/dist/wearable", "tau-list/project/lib/tau")
-    		], ["cd web-ui-fw/tau", "grunt build"],
+    		], ["cd web-ui-fw/tau", "npm install", "grunt build"],
     		[
     			SrcDest("web-ui-fw/tau/dist/VERSION", "wearable-widget-sample/project/lib/tau/VERSION"),
     			SrcDest("web-ui-fw/tau/dist/LICENSE.MIT", "wearable-widget-sample/project/lib/tau/LICENSE.MIT"),
