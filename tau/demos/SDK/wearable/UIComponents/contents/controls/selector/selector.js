ae7e1aeee6b18d23a4a2dab9eaea60482b743fa9
Hyeoncheol <hc7.choi@samsung.com>
2015-07-09 13:56:00 +0900

    fix(wearable): modify custom indicator sample of Selector
    Add z-index value to custom indicator
    
    Change-Id: Iefc4665ad606bbd3ab1117aef3218d1c99b2b456
    Signed-off-by: Hyeoncheol <hc7.choi@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/contents/controls/selector/selector.js
+++ tau/demos/SDK/wearable/UIComponents/contents/controls/selector/selector.js
@@ -5,8 +5,17 @@
 		clickBound;
 
 	function onClick(event) {
-		var activeItem = selector.querySelector(".ui-item-active");
+		var activeItem = selector.querySelector(".ui-item-active"),
+			target = event.target;;
 		//console.log(activeItem.getAttribute("data-title"));
+		/*
+		 * Default indicator class selector is "ui-selector-indicator".
+		 * If you want to show custom indicator sample code,
+		 * check the 'customIndicator.js' please.
+		 */
+		if (target.classList.contains("ui-selector-indicator")) {
+			//console.log("Indicator clicked");
+		}
 	}
 	page.addEventListener("pagebeforeshow", function() {
 		clickBound = onClick.bind(null);
