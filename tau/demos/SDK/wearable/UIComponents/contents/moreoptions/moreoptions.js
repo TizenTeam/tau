8e0402e8158c8c2ccc630c7b3cb62b7df88ced93
Hyeoncheol <hc7.choi@samsung.com>
2015-07-10 11:47:32 +0900

    fix(wearable): modify Selector with drawer sample UX
    Drawer close when user click the indicator of Selector
    
    Change-Id: Ia132eab564ef798020ce0b89987002bd1f340ffb
    Signed-off-by: Hyeoncheol <hc7.choi@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/contents/moreoptions/moreoptions.js
+++ tau/demos/SDK/wearable/UIComponents/contents/moreoptions/moreoptions.js
@@ -31,4 +31,18 @@
 			helper.destroy();
 		}
 	});
+	/*
+	 * When user click the indicator of Selector, drawer will close.
+	 */
+	selector.addEventListener("click", function(event) {
+		var target = event.target,
+			drawerComponent = tau.widget.Drawer(drawer);
+
+		if (tau.support.shape.circle) {
+			// 'ui-selector-indicator' is default indicator class name of Selector component
+			if (target.classList.contains("ui-selector-indicator")) {
+				drawerComponent.close();
+			}
+		}
+	});
 }());
