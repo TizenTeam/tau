4dd06ecaa30c6b2a7f5fe63ad90318ed3745e2a1
Hyeoncheol <hc7.choi@samsung.com>
2015-07-17 16:44:39 +0900

    fix(wearable): add enable/disable to Selector
    Change-Id: I271972bcce7c24937931634a8816a9b3b7012c38
    Signed-off-by: Hyeoncheol <hc7.choi@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/contents/list/list_selectmode.js
+++ tau/demos/SDK/wearable/UIComponents/contents/list/list_selectmode.js
@@ -13,6 +13,7 @@
 		drawerElement = page.querySelector("#rightDrawer"),
 		handler = document.getElementById("handler"),
 		selector = page.querySelector("#selector"),
+		selectorComponent,
 		selectCount,
 		drawerHelper,
 		i,
@@ -121,9 +122,22 @@
 		drawerHelper = tau.helper.DrawerMoreStyle.create(drawerElement, {
 			handler: ".drawer-handler"
 		});
+		selectorComponent = tau.widget.Selector(selector);
+		selectorComponent.disable();
 		document.addEventListener('tizenhwkey', fnBackKey);
 	});
 
+	/*
+	 * If you want to use Selector with Snaplistview, you should control to Selector enable status
+	 * because 'rotarydetent' event has been used in both Selector and Snaplistview.
+	 */
+	drawerElement.addEventListener("draweropen", function() {
+		selectorComponent.enable();
+	});
+
+	drawerElement.addEventListener("drawerclose", function() {
+		selectorComponent.disable();
+	});
 	/*
 	 * When user click the indicator of Selector, drawer will close.
 	 */
