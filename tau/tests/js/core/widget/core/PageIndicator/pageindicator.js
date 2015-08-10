ae19303e54362b483f20cee468e71c060a138cc9
Tomasz Lukawski <t.lukawski@samsung.com>
2015-08-10 10:13:49 +0200

    fix(wearable) tests: PageIndicator - test was changed to asynchronous
    Change-Id: Ifdfa891e88ac218e5928c77a28dcc014f50d19ef
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/core/widget/core/PageIndicator/pageindicator.js
+++ tau/tests/js/core/widget/core/PageIndicator/pageindicator.js
@@ -1,47 +1,51 @@
 module("tau.widget.core.PageIndicator", {});
 
 var pageWidget = document.getElementById("pageIndicatorPage");
-pageWidget.addEventListener("pageshow", function() {
-    test("tau.widget.core.PageIndicator Test", function() {
-        var elPageIndicator1 = document.getElementById("pageIndicator1"),
-            elPageIndicator2 = document.getElementById("pageIndicator2"),
-            pageIndicator1 = tau.widget.PageIndicator(elPageIndicator1),
-            pageIndicator2 = tau.widget.PageIndicator(elPageIndicator2),
-            i,pos;
 
-        function checkMarkUp(widget) {
-            var i,
-                element = widget.element,
-                len = element.children.length;
-            ok(element.classList.contains("ui-page-indicator"), "PageIndicator has ui-page-indicator class");
+asyncTest("tau.widget.core.PageIndicator Test", function() {
+	document.addEventListener("pageshow", function() {
+		var elPageIndicator1 = document.getElementById("pageIndicator1"),
+			elPageIndicator2 = document.getElementById("pageIndicator2"),
+			pageIndicator1 = tau.widget.PageIndicator(elPageIndicator1),
+			pageIndicator2 = tau.widget.PageIndicator(elPageIndicator2),
+			i,pos;
 
-            for(i=0; i<len; i++) {
-                ok(element.children[i].classList.contains("ui-page-indicator-item"), "Dot has ui-page-indicator-item class");
-            }
-        }
+		function checkMarkUp(widget) {
+			var i,
+				element = widget.element,
+				len = element.children.length;
+			ok(element.classList.contains("ui-page-indicator"), "PageIndicator has ui-page-indicator class");
 
-        // # of pages < maximum # of dots
-        checkMarkUp(pageIndicator1);
-        for(i=0;i<3;i++) {
-            pageIndicator1.setActive(i);
-            ok(elPageIndicator1.children[i].classList.contains("ui-page-indicator-active"), "corresponding dot is active");
-        }
-        equal(elPageIndicator1.children.length, pageIndicator1.options.numberOfPages, "ok");
+			for(i=0; i<len; i++) {
+				ok(element.children[i].classList.contains("ui-page-indicator-item"), "Dot has ui-page-indicator-item class");
+			}
+		}
 
-        // # of pages > maximum # of dots
-        checkMarkUp(pageIndicator2);
-        for(i=0;i<10;i++) {
-            pos = 0;
-            pageIndicator2.setActive(i);
-            if(i < 3) {
-                pos = i;
-            } else if (i >= 3 && i < 8) {
-                pos = 2;
-            } else {
-                pos = i - 5;
-            }
-            ok(elPageIndicator2.children[pos].classList.contains("ui-page-indicator-active"), "corresponding dot is active");
-        }
-        equal(elPageIndicator2.children.length, pageIndicator2._getMaxPage(), "ok");
-    });
-}, false);
+		// # of pages < maximum # of dots
+		checkMarkUp(pageIndicator1);
+		for(i=0;i<3;i++) {
+			pageIndicator1.setActive(i);
+			ok(elPageIndicator1.children[i].classList.contains("ui-page-indicator-active"), "corresponding dot is active");
+		}
+		equal(elPageIndicator1.children.length, pageIndicator1.options.numberOfPages, "ok");
+
+		// # of pages > maximum # of dots
+		checkMarkUp(pageIndicator2);
+		for(i=0;i<10;i++) {
+			pos = 0;
+			pageIndicator2.setActive(i);
+			if(i < 3) {
+				pos = i;
+			} else if (i >= 3 && i < 8) {
+				pos = 2;
+			} else {
+				pos = i - 5;
+			}
+			ok(elPageIndicator2.children[pos].classList.contains("ui-page-indicator-active"), "corresponding dot is active");
+		}
+		equal(elPageIndicator2.children.length, pageIndicator2._getMaxPage(), "ok");
+		start();
+	}, true);
+
+	tau.engine.run();
+});
