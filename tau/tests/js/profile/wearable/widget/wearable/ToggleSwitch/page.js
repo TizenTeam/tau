7ab2d8616cb7d15fed1540380af46550ddff8f88
Tomasz Lukawski <t.lukawski@samsung.com>
2015-08-10 10:21:09 +0200

    fix(wearable) tests: ToggleSwitch - change test start on pagebeforeshow event
    Change-Id: I95a19813460ab342f85384a5ddc92cc86411cd30
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/profile/wearable/widget/wearable/ToggleSwitch/page.js
+++ tau/tests/js/profile/wearable/widget/wearable/ToggleSwitch/page.js
@@ -1,8 +1,9 @@
 module("profile/wearable/widget/wearable/ToggleSwitch", {});
 
 var pageWidget = document.getElementById('first');
-pageWidget.addEventListener('pageshow', function() {
-	test("profile/wearable/widget/wearable/ToggleSwitch _build method", function () {
+
+asyncTest("profile/wearable/widget/wearable/ToggleSwitch _build method", function () {
+	pageWidget.addEventListener('pageshow', function() {
 		var switchElement = document.getElementById('switch'),
 			switchWidget = tau.widget.ToggleSwitch(switchElement);
 		equal(switchElement.children[0].className, "ui-switch-text", 'text element has proper class');
@@ -12,5 +13,8 @@ pageWidget.addEventListener('pageshow', function() {
 		equal(switchElement.children[1].children.length, 2, 'label element has proper number of child');
 		equal(switchElement.children[1].children[0].className, "ui-switch-input", 'input element has proper class');
 		equal(switchElement.children[1].children[1].className, "ui-switch-activation", 'activation element has proper class');
-	});
-}, false);
+		start();
+	}, true);
+
+	tau.engine.run();
+});
