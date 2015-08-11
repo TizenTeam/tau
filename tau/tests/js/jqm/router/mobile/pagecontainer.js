e2acdfede804472a9d7ee88b8566676e98f87631
Krzysztof Antoszek <k.antoszek@samsung.com>
2015-08-11 11:28:17 +0200

    fix(mobile): Fixed pagecontainer test
    Change-Id: I680791e2037bd0716e0b539fcbfcd7ec8c70d7be
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

--- tau/tests/js/jqm/router/mobile/pagecontainer.js
+++ tau/tests/js/jqm/router/mobile/pagecontainer.js
@@ -1,19 +1,29 @@
-module("jqm/router");
+(function (document, $, q) {
+	"use strict";
 
-$(document).one('pagechange', function(event) {
-	asyncTest("pageContainer", 5, function () {
-		var pageContainer = document.getElementById('pageContainer1');
-		ok(!document.body.classList.contains('ui-mobile-viewport'), 'body not contains ui-mobile-viewport');
-		ok(pageContainer.classList.contains('ui-mobile-viewport'), 'pageContainer contains ui-mobile-viewport');
-		ok(document.body.className.indexOf('ui-overlay') === -1, 'body not contains overlay');
-		ok(pageContainer.className.indexOf('ui-overlay') > -1, 'pageContainer contains overlay');
-		window.setTimeout(function () {
-			start();
-		}, 2000);
-		$(pageContainer).bind('pageshow', function (event) {
-			ok(event.target, 'call pageshow');
-		});
-		document.getElementById("btn1").click();
-		document.getElementById("btn2").click();
+	q.module("jqm/router", {
+		teardown: function () {
+			document.location.hash = "";
+		}
 	});
-});
+
+	q.asyncTest("pageContainer", 6, function () {
+		$(document).one('pageshow', function(event) {
+			var pageContainer = document.getElementById('pageContainer1');
+
+			q.ok(!document.body.classList.contains('ui-mobile-viewport'), 'body not contains ui-mobile-viewport');
+			q.ok(pageContainer.classList.contains('ui-mobile-viewport'), 'pageContainer contains ui-mobile-viewport');
+			q.ok(document.body.className.indexOf('ui-overlay') === -1, 'body not contains overlay');
+			q.ok(pageContainer.className.indexOf('ui-overlay') > -1, 'pageContainer contains overlay');
+
+			$(pageContainer).bind('pageshow', function (event) {
+				q.ok(event.target, 'call pageshow');
+				q.equal(event.target.id, "page3", "correct page shown");
+				start();
+			});
+
+			document.getElementById("btn1").click();
+			document.getElementById("btn2").click();
+		});
+	});
+}(window.document, jQuery, QUnit));
