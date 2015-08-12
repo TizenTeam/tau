fb2043ddb0a3de808abd08862bc3637a00ab25a6
Krzysztof Antoszek <k.antoszek@samsung.com>
2015-08-12 11:44:36 +0200

    fix(mobile): Fixed pagecontainer test
    Change-Id: I72880fc825ea90e15a54d621c9d09371a7317967
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

--- tau/tests/js/jqm/router/mobile/pagecontainer.js
+++ tau/tests/js/jqm/router/mobile/pagecontainer.js
@@ -8,22 +8,29 @@
 	});
 
 	q.asyncTest("pageContainer", 6, function () {
-		$(document).one('pageshow', function(event) {
-			var pageContainer = document.getElementById('pageContainer1');
+		var test = function () {
+					var pageContainer = document.getElementById('pageContainer1');
 
-			q.ok(!document.body.classList.contains('ui-mobile-viewport'), 'body not contains ui-mobile-viewport');
-			q.ok(pageContainer.classList.contains('ui-mobile-viewport'), 'pageContainer contains ui-mobile-viewport');
-			q.ok(document.body.className.indexOf('ui-overlay') === -1, 'body not contains overlay');
-			q.ok(pageContainer.className.indexOf('ui-overlay') > -1, 'pageContainer contains overlay');
+					q.ok(!document.body.classList.contains('ui-mobile-viewport'), 'body not contains ui-mobile-viewport');
+					q.ok(pageContainer.classList.contains('ui-mobile-viewport'), 'pageContainer contains ui-mobile-viewport');
+					q.ok(document.body.className.indexOf('ui-overlay') === -1, 'body not contains overlay');
+					q.ok(pageContainer.className.indexOf('ui-overlay') > -1, 'pageContainer contains overlay');
 
-			$(pageContainer).bind('pageshow', function (event) {
-				q.ok(event.target, 'call pageshow');
-				q.equal(event.target.id, "page3", "correct page shown");
-				start();
-			});
+					$(pageContainer).bind('pageshow', function (event) {
+						q.ok(event.target, 'call pageshow');
+						q.equal(event.target.id, "page3", "correct page shown");
+						start();
+					});
 
-			document.getElementById("btn1").click();
-			document.getElementById("btn2").click();
-		});
+					document.getElementById("btn1").click();
+					document.getElementById("btn2").click();
+				},
+				active = $(".ui-page-active");
+
+		if (active && $(active).attr("id") === "page1") {
+			test();
+		} else {
+			$(document).one("pageshow", test);
+		}
 	});
 }(window.document, jQuery, QUnit));
