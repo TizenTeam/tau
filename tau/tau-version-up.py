c3fc6dc0c73aec1017a0014ca3463e237acb7f2c
hagun.kim <hagun.kim@samsung.com>
2016-01-25 14:17:25 +0900

    etc(tool): update release script
    - tau-version-up script is added.
    	Usage:
    	./tau-version-up.py [TAU version] [Git account ID] '[Real name for changelog]'
    - In new release process, since it is necessary to update sample version,
     updateSampleVersion is added into the script.
    - It is applied to only samples not templates.
    - The sample versioning is "X.X.XX". To avoid confusing, the last XX is started at 10.
    - Since there is no wearable profile in 2.4, all works for wearable are removed.
    
    Change-Id: I7deb1bdbbd3c29e1bda261ad2c078a050c728ed3
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

---
+++ tau/tau-version-up.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# author Hagun Kim <hagun.kim@samsung.com>
+# This script is used for update TAU version in own git
+import os, sys, subprocess, shlex, datetime
+
+gitaccount=""
+tauVersion=""
+userName=""
+FILE_CHANGELOG = "../packaging/changelog"
+FILE_SPEC = "../packaging/web-ui-fw.spec"
+FILE_PACKAGEJSON = "./package.json"
+
+if(len(sys.argv) < 4):
+	print("Please input TAU version, account ID, Real name for changelog\nex] ./tau-version-up.py 0.11.5 hagun.kim 'Hagun Kim'")
+	sys.exit(1)
+
+if(len(sys.argv) == 4):
+	tauVersion=sys.argv[1]
+	gitaccount=sys.argv[2]
+	userName=sys.argv[3]
+
+def cmd(command):
+	return subprocess.call(shlex.split(command))
+
+def pushGit():
+	# make commit and push to gerrit
+	cmd("git add " + FILE_CHANGELOG + " " + FILE_SPEC + " " + FILE_PACKAGEJSON)
+	logmsg = "TAU " + tauVersion + " release"
+
+	cmd("git commit -s -m '" + logmsg + "'")
+	cmd("git push origin HEAD:refs/for/devel/tizen_2.4")
+
+def addReviewer():
+	targetCommit = subprocess.check_output("git log --format='%H' -n 1", shell=True).replace("\n", "")
+	setReviewerCommand = "ssh -p 29418 " + gitaccount + "@165.213.149.170 gerrit set-reviewers -a hk0713.cho@samsung.com "
+	cmd(setReviewerCommand + targetCommit)
+
+def modifyFile(filePath, textParam):
+	f = open(filePath, "r")
+	fText = f.readlines()
+	f.close()
+
+	if (filePath == FILE_CHANGELOG):
+		fText.insert(1, textParam)
+	elif (filePath == FILE_SPEC):
+		fText[1] = "Version:     " + tauVersion + "\n"
+	else:
+		fText[4] = '  "version": "' + tauVersion + '",\n'
+
+	fText = "".join(fText)
+	f = open(filePath, "w")
+	f.write(fText)
+	f.close()
+
+def main():
+	latestTag = subprocess.check_output("git describe --abbrev=0 --tags", shell=True).replace("\n", "")
+	gitLogCommand = 'git log --pretty=oneline --no-merges ' + latestTag + '..HEAD | sed -e "s/^\S* /- /g"'.replace("-", "\t-")
+
+	gitLog = os.popen(gitLogCommand).read()
+	now = datetime.datetime.now().strftime("%a %b %d %Y")
+
+	gitLog = "* " + now + " " + userName + " <" + gitaccount + "@samsung.com> " + tauVersion + "\n" + gitLog + "\n"
+
+	print("Update changelog file")
+	modifyFile(FILE_CHANGELOG, gitLog)
+	print("Update web-ui-fw.spec file")
+	modifyFile(FILE_SPEC, False)
+	print("Update package.json file")
+	modifyFile(FILE_PACKAGEJSON, False)
+
+	pushGit()
+
+	addReviewer()
+
+	print("GIT push completed. Please merge and tag.")
+
+if __name__ == "__main__":
+	main()
--- tau/update-tau-to-sdk.py
+++ tau/update-tau-to-sdk.py
@@ -5,6 +5,7 @@
 # If you have any problem with this script, please contact Heeju Joo <heeju.joo@samsung.com>
 import os, sys, subprocess, shutil, fileinput
 import xml.etree.ElementTree as ET
+import jenkins
 
 cwd=os.getcwd()
 tempdir=cwd+"/_temp"
@@ -44,64 +45,48 @@ class Job:
 
 # Git info
 class webuifw(Git):
-	addr="168.219.209.56:29418/framework/web/web-ui-fw"
+	addr="165.213.149.170:29418/framework/web/web-ui-fw"
 	branch="devel/tizen_2.4"
 
 class MobileUIComponent(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/sample/tizen-winset"
+	addr="165.213.149.170:29418/apps/mobile/web/sample/tizen-winset"
 	branch="tizen_2.4"
+	type="onlineSample"
 
 class TizenGlobalize(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/sample/tizen-globalize"
+	addr="165.213.149.170:29418/apps/mobile/web/sample/tizen-globalize"
 	branch="tizen_2.4"
+	type="onlineSample"
 
 class TAUMasterDetail(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/template/tau-master-detail"
+	addr="165.213.149.170:29418/apps/mobile/web/template/tau-master-detail"
 	branch="tizen_2.4"
+	type="template"
 
 class TAUMultiPage(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/template/tau-multi-page"
+	addr="165.213.149.170:29418/apps/mobile/web/template/tau-multi-page"
 	branch="tizen_2.4"
-
-class TAUNavigationView(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/template/tau-navigation-view"
-	branch="tizen_2.4"
+	type="template"
 
 class TAUSinglePage(Git):
-	addr="168.219.209.56:29418/apps/mobile/web/template/tau-single-page"
+	addr="165.213.149.170:29418/apps/mobile/web/template/tau-single-page"
 	branch="tizen_2.4"
-
-class WearableUIComponent(Git):
-	addr="168.219.209.56:29418/apps/wearable/web/sample/wearable-widget-sample"
-	branch="tizen_2.4"
-
-class TAUBasic(Git):
-	addr="168.219.209.56:29418/apps/wearable/web/template/tau-basic"
-	branch="tizen_2.4"
-
-class TAUList(Git):
-	addr="168.219.209.56:29418/apps/wearable/web/template/tau-list"
-	branch="tizen_2.4"
-
-class TAUAnimation(Git):
-	addr="168.219.209.56:29418/apps/wearable/web/sample/tizen-animation"
-	branch="tizen_2.4"
+	type="template"
 
 # job list
 jobs = {
 	"mobile": Job(
 		webuifw,
-		[MobileUIComponent, TizenGlobalize, TAUMasterDetail, TAUMultiPage, TAUNavigationView, TAUSinglePage],
+		[MobileUIComponent, TizenGlobalize, TAUMasterDetail, TAUMultiPage, TAUSinglePage],
 		[
 			SrcDest("web-ui-fw/tau/demos/SDK/mobile/UIComponents", "tizen-winset/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tizen-winset/project/lib/tau"),
+			SrcDest("web-ui-fw/tau/dist/animation", "tizen-winset/project/lib/tau/animation"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tizen-globalize/project/lib/tau/mobile"),
 			SrcDest("web-ui-fw/tau/demos/SDK/mobile/MasterDetail", "tau-master-detail/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-master-detail/project/lib/tau"),
 			SrcDest("web-ui-fw/tau/demos/SDK/mobile/MultiPage", "tau-multi-page/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-multi-page/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/mobile/NavigationView", "tau-navigation-view/project"),
-			SrcDest("web-ui-fw/tau/dist/mobile", "tau-navigation-view/project/lib/tau"),
 			SrcDest("web-ui-fw/tau/demos/SDK/mobile/SinglePage", "tau-single-page/project"),
 			SrcDest("web-ui-fw/tau/dist/mobile", "tau-single-page/project/lib/tau")
 		], ["cd web-ui-fw/tau", "npm install", "grunt build"],
@@ -114,34 +99,8 @@ jobs = {
 			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-master-detail/project/lib/tau/LICENSE.Flora"),
 			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-multi-page/project/lib/tau/VERSION"),
 			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-multi-page/project/lib/tau/LICENSE.Flora"),
-			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-navigation-view/project/lib/tau/VERSION"),
-			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-navigation-view/project/lib/tau/LICENSE.Flora"),
 			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-single-page/project/lib/tau/VERSION"),
 			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-single-page/project/lib/tau/LICENSE.Flora")
-		]),
-	"wearable": Job(
-		webuifw,
-		[WearableUIComponent, TAUBasic, TAUList, TAUAnimation],
-		[
-			SrcDest("web-ui-fw/tau/demos/SDK/wearable/UIComponents", "wearable-widget-sample/project"),
-			SrcDest("web-ui-fw/tau/dist/wearable", "wearable-widget-sample/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/wearable/Basic", "tau-basic/project"),
-			SrcDest("web-ui-fw/tau/dist/wearable", "tau-basic/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/wearable/List", "tau-list/project"),
-			SrcDest("web-ui-fw/tau/dist/wearable", "tau-list/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/demos/SDK/wearable/Animation", "tizen-animation/project"),
-			SrcDest("web-ui-fw/tau/dist/wearable", "tizen-animation/project/lib/tau"),
-			SrcDest("web-ui-fw/tau/dist/animation", "tizen-animation/project/lib/tau/animation")
-		], ["cd web-ui-fw/tau", "npm install", "grunt build"],
-		[
-			SrcDest("web-ui-fw/tau/dist/VERSION", "wearable-widget-sample/project/lib/tau/VERSION"),
-			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "wearable-widget-sample/project/lib/tau/LICENSE.Flora"),
-			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-basic/project/lib/tau/VERSION"),
-			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-basic/project/lib/tau/LICENSE.Flora"),
-			SrcDest("web-ui-fw/tau/dist/VERSION", "tau-list/project/lib/tau/VERSION"),
-			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tau-list/project/lib/tau/LICENSE.Flora"),
-			SrcDest("web-ui-fw/tau/dist/VERSION", "tizen-animation/project/lib/tau/VERSION"),
-			SrcDest("web-ui-fw/tau/dist/LICENSE.Flora", "tizen-animation/project/lib/tau/LICENSE.Flora")
 		])
 }
 
@@ -175,6 +134,9 @@ def pushGit(targetdir, tauVersion):
 	root = tree.getroot()
 	projectName = root.find('SampleName').text
 
+	if (targetdir.find("tizen-") > -1):
+		updateSampleVersion(tree, root)
+
 	# make commit and push to gerrit
 	cmd("git add -A")
 	logmsg = projectName.replace(" ", "") + "-TAU(" + tauVersion + ")release"
@@ -182,7 +144,46 @@ def pushGit(targetdir, tauVersion):
 	cmd("git commit -m " + logmsg)
 	cmd("git push origin HEAD:refs/for/tizen_2.4")
 
+	targetCommit = subprocess.check_output("git log --format='%H' -n 1", shell=True).replace("\n", "")
+	cmd("ssh -p 29418 " + gitaccount + "@165.213.149.170 gerrit review --verified +1 --code-review +2 submit " + targetCommit)
+
 	os.chdir(cwd)
+
+def updateSampleVersion(tree, root):
+	current_version = root.findtext("SampleVersion")
+
+	split_version = current_version.split(".")
+
+	for i in range(len(split_version)-1, -1, -1):
+		split_version[i] = int(split_version[i])
+		if (split_version[i] < 99 and split_version[i] > 9 and i == 2) or (split_version[i] < 9 and i == 1) or i == 0:
+			split_version[i] += 1
+			break
+		elif i == 2 and split_version[i] < 10:
+			split_version[i] = 10
+			break
+		elif i == 2:
+			split_version[i] = 10
+			continue
+		elif i == 1:
+			split_version[i] = 0
+			continue
+
+	changed_version = str(split_version[0]) + "." + str(split_version[1]) + "." + str(split_version[2])
+
+	for version in root.iter("SampleVersion"):
+		version.text = changed_version
+
+	tree.write(FILE_XML, encoding="utf-8", xml_declaration=True)
+
+def executeJenkinsJobs(git):
+	sampleGitPath = git.addr.replace("165.213.149.170:29418/", "")
+	jenkinsServer = jenkins.Jenkins('http://10.113.63.84:8080', username='sample', password='sample')
+
+	print("[Jenkins job] upload to spin " + os.path.basename(git.addr) + " / " + git.branch)
+	jenkinsServer.build_job('online_sample_upload_to_spin', {'sample_git_path': sampleGitPath, 'branch_name': git.branch})
+	print("[Jenkins job] copy to stable " + os.path.basename(git.addr) + " / " + git.branch)
+	jenkinsServer.build_job('online_sample_copy_to_stable', {'snapshot_name': '', 'sample_list': sampleGitPath + "," + git.branch})
 
 def main():
 	global tempdir
@@ -257,6 +258,10 @@ def main():
 		projectdir = os.listdir(jobdir)
 		for project in projectdir:
 			pushGit(project, tauVersion.strip())
+
+		for git in job.destgit:
+			if git.type == "onlineSample":
+				executeJenkinsJobs(git)
 
 		os.chdir(tempdir)
 	os.chdir(cwd)
