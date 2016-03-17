73d85d96d6990a7a639dab0b61ceaf141e479b68
hagun.kim <hagun.kim@samsung.com>
2016-03-17 20:00:17 +0900

    refactor(wearable): modify section changer sample
    - Section changer with tab is removed.
    - Page indicator is added to the sample.
    
    Change-Id: Ied8576c846c079e6ae8fc6a2356baf1afc23a665
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/contents/navcontrols/sectionchanger/circular.js
+++ tau/demos/SDK/wearable/UIComponents/contents/navcontrols/sectionchanger/circular.js
@@ -3,9 +3,16 @@
 
 var page = document.getElementById( "circularSectionchangerPage" ),
 	changer = document.getElementById( "circularSectionchanger" ),
-	sectionChanger;
+	sectionLength = document.querySelectorAll("section").length,
+	elPageIndicator = document.getElementById("pageIndicator"),
+	sectionChanger,
+	pageIndicator,
+	pageIndicatorHandler;
 
 page.addEventListener( "pagebeforeshow", function() {
+	// make PageIndicator
+	pageIndicator =  tau.widget.PageIndicator(elPageIndicator, { numberOfPages: sectionLength });
+	pageIndicator.setActive(2);
 	// make SectionChanger object
 	sectionChanger = tau.widget.SectionChanger(changer, {
 		circular: true,
@@ -17,4 +24,10 @@ page.addEventListener( "pagehide", function() {
 	// release object
 	sectionChanger.destroy();
 });
+
+pageIndicatorHandler = function (e) {
+	pageIndicator.setActive(e.detail.active);
+};
+
+changer.addEventListener("sectionchange", pageIndicatorHandler, false);
 }());
--- tau/demos/SDK/wearable/UIComponents/contents/navcontrols/sectionchanger/hsection.js
+++ tau/demos/SDK/wearable/UIComponents/contents/navcontrols/sectionchanger/hsection.js
@@ -3,9 +3,16 @@
 
 var page = document.getElementById( "hsectionchangerPage" ),
 	changer = document.getElementById( "hsectionchanger" ),
-	sectionChanger;
+	sectionLength = document.querySelectorAll("section").length,
+	elPageIndicator = document.getElementById("pageIndicator"),
+	sectionChanger,
+	pageIndicator,
+	pageIndicatorHandler;
 
 page.addEventListener( "pagebeforeshow", function() {
+	// make PageIndicator
+	pageIndicator =  tau.widget.PageIndicator(elPageIndicator, { numberOfPages: sectionLength });
+	pageIndicator.setActive(2);
 	// make SectionChanger object
 	sectionChanger = tau.widget.SectionChanger(changer, {
 		circular: false,
@@ -18,5 +25,11 @@ page.addEventListener( "pagehide", function() {
 	// release object
 	sectionChanger.destroy();
 });
+
+pageIndicatorHandler = function (e) {
+	pageIndicator.setActive(e.detail.active);
+};
+
+changer.addEventListener("sectionchange", pageIndicatorHandler, false);
 
 }());
