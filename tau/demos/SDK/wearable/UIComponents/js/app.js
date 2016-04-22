f6ed55aa6090cf22ff3fabf0aaf8c923acdb637f
hagun.kim <hagun.kim@samsung.com>
2016-04-22 14:49:09 +0900

    refactor(wearable): modify wearable UI Components sample for consistency
    Change-Id: I4b3130d3033fe363b9a52cd6345a52d4e77adc92
    Signed-off-by: hagun.kim <hagun.kim@samsung.com>

--- tau/demos/SDK/wearable/UIComponents/js/app.js
+++ tau/demos/SDK/wearable/UIComponents/js/app.js
@@ -1,12 +1,12 @@
-( function () {
+(function() {
 	/**
 	 * Back key event handler
 	 */
-	window.addEventListener( 'tizenhwkey', function( ev ) {
-		if( ev.keyName === "back" ) {
-			var page = document.getElementsByClassName( 'ui-page-active' )[0],
+	window.addEventListener('tizenhwkey', function(ev) {
+		if (ev.keyName === "back") {
+			var page = document.getElementsByClassName('ui-page-active')[0],
 				pageid = page ? page.id : "";
-			if( pageid === "main" ) {
+			if (pageid === "main") {
 				try {
 					tizen.application.getCurrentApplication().exit();
 				} catch (ignore) {
@@ -15,5 +15,5 @@
 				window.history.back();
 			}
 		}
-	} );
-} () );
+	});
+}());
