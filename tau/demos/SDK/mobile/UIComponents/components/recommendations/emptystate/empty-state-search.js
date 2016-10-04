9e3a202c0e2e7729d52a13cb668c477e4d5babd8
Tomasz Lukawski <t.lukawski@samsung.com>
2016-10-04 16:36:32 +0200

    fix(mobile) Empty state page - fixing
    Issue: OAPWAF-1601
    test:
    - 165-empty-state
    - 166-empty-state-with-floating-action
    - 168-empty-state-search
    
    and added new examples to UIComponents app.
    
    Moved welcome page to recommendations.
    
    Change-Id: I88234d535dae97edf97a744426fc05c8ea4e8831
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>
    Signed-off-by: Piotr Karny <p.karny@samsung.com>

---
+++ tau/demos/SDK/mobile/UIComponents/components/recommendations/emptystate/empty-state-search.js
@@ -0,0 +1,79 @@
+(function(tau) {
+    var page = document.getElementById("page-empty-state-search"),
+        search =  document.getElementById("search-input"),
+        list = document.getElementById("searchList"),
+        listItems = list.querySelectorAll("[data-filtertext]"),
+        emptyStateStyle = document.querySelector(".ui-empty-state-content").style,
+        listItemsArray = [].slice.call(listItems),
+        searchHandlerBound,
+        searchClearBound;
+
+    function updateEmptyState() {
+        var itemsCount = list.querySelectorAll("li").length;
+
+        // show empty state if no results found
+        if (itemsCount > 0 &&
+            list.querySelectorAll(".li-search-hidden").length === itemsCount) {
+            emptyStateStyle.display = null;
+        } else {
+            emptyStateStyle.display = "none";
+        }
+    }
+
+    /**
+     * Shows items that match the entered value
+     * keyup event handler
+     */
+    function searchHandler() {
+        var itemsCount = list.querySelectorAll("li").length;
+
+        listItemsArray.forEach(function(item) {
+            var itemText = item.getAttribute("data-filtertext");
+            if ( itemText.toString().toLowerCase().indexOf(search.value.toLowerCase()) === -1 ) {
+                item.classList.add("li-search-hidden");
+            } else {
+                item.classList.remove("li-search-hidden");
+            }
+        });
+
+        updateEmptyState();
+    }
+
+    /**
+     * Initializes search result
+     */
+    function searchClear() {
+        if(search.value === "") {
+            listItemsArray.forEach(function(item) {
+                item.classList.remove("li-search-hidden");
+            });
+        }
+
+        updateEmptyState();
+    }
+
+    /**
+     * pagebeforeshow event handler
+     * Do preparatory works and adds event listeners
+     */
+    page.addEventListener("pagebeforeshow", function() {
+        searchHandlerBound = searchHandler.bind(this);
+        searchClearBound = searchClear.bind(this);
+        search.addEventListener("keyup", searchHandlerBound, false);
+        search.addEventListener("search", searchClearBound, false);
+    });
+
+    page.addEventListener("pageshow", function() {
+        tau.event.trigger(search, "keyup");
+    });
+
+    /**
+     * pagehide event handler
+     * Destroys and removes event listeners
+     */
+    page.addEventListener("pagehide", function() {
+        search.removeEventListener("keyup", searchHandlerBound, false);
+        search.removeEventListener("search", searchClearBound, false);
+    });
+
+}(window.tau));
