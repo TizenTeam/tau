9717e367c2b45c30f0e5fc6a33f17747f6fb5017
Krzysztof Antoszek <k.antoszek@samsung.com>
2016-11-25 15:40:13 +0100

    fix(mobile): UIComponents changed selectlist to ui-flex type
    Change-Id: I8f6ac584b9c82b737728c2305a4b23f4306cf9cf
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

--- tau/demos/SDK/mobile/UIComponents/components/list/list-select-check.js
+++ tau/demos/SDK/mobile/UIComponents/components/list/list-select-check.js
@@ -1,20 +1,44 @@
-(function() {
-	var list = document.getElementById("select-list"),
-		checkboxes = list.querySelectorAll("input[type='checkbox']"),
-		elSelectAll = document.getElementById("select-all"),
-		isAll = false;
+(function(tau) {
+	"use strict";
+	// get the page
+	var page = document.getElementById("select-all-page");
 
-	/**
-	 * Selects/deselects all items
-	 */
-	function selectAll() {
-		var i, len;
-		for(i=0,len=checkboxes.length;i<len;i++) {
-			checkboxes[i].checked = !isAll;
+	// handle 'change' event
+	function handleChange(e) {
+		var checkbox = e.target,
+			checked = e.target.checked,
+			parent = tau.util.selectors.getClosestByTag(checkbox, "li"), // get parent li element
+			next = null,
+			childCheckbox = null;
+
+		// check if 'change' event originated in a ui-group-index element
+		if (parent && parent.classList.contains("ui-group-index")) {
+			next = parent.nextElementSibling; // set next sibling as next element
+			// iterate over next li elements and stop when a second group index
+			// element is found
+			while (next && next.classList.contains("ui-group-index") === false) {
+				// find the input element in li element
+				childCheckbox = next.querySelector("input[type=checkbox]");
+				if (childCheckbox) {
+					// change checked status according to originating group index input
+					// element checked flag
+					childCheckbox.checked = checked;
+				}
+				// mark next element for iteration
+				next = next.nextElementSibling;
+			}
 		}
-		isAll = !isAll;
 	}
 
-	elSelectAll.addEventListener("change", selectAll);
-}());
+	// initialize the script
+	function init(event) {
+		// watch for 'change' events only in current page
+		tau.event.on(event.target, "change", handleChange);
+	}
+
+	if (page) {
+		// run init only once
+		tau.event.one(page, "pageshow", init);
+	}
+}(window.tau));
 
