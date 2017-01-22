52f6d7629caf067bcd5fbe52c181ddefb00e1b45
Maciej Urbanski <m.urbanski@samsung.com>
2017-01-22 09:38:16 +0100

    tool(release): Refactor script to version up [OAPTAU-121]
     - add helper to clearly define flow which can be used in other scripts
    
    Change-Id: I21c47ed90f86e5331f6ca50feabede679e6a9fbc

--- tau/tau-version-up.js
+++ tau/tau-version-up.js
@@ -1,132 +1,190 @@
 #!/usr/bin/env node
 
 var exec = require("child_process").exec,
+	async = require("async"),
 	moment = require("moment"),
 	fs = require("fs"),
+	env = process.env,
 	FILE_CHANGELOG = "../packaging/changelog",
 	FILE_SPEC = "../packaging/web-ui-fw.spec",
 	FILE_PACKAGEJSON = "./package.json",
-	cmd = {
-		/**
-		 * Run one command
-		 * @param command
-		 * @param callback
-		 */
-		get: function (command, callback) {
-			exec(
-				command,
-				function (err, data, stderr) {
-					if (err) {
-						console.error("exec error: " + err);
-					}
-					if (stderr) {
-						console.error(stderr);
-					}
-					callback(data);
+	gitAccount = "",
+	userName = "",
+	lastTag = "",
+	tauVersion = env.VERSION || env.bamboo_deploy_release || env.bamboo_jira_version,
+	commitMessage = "TAU " + tauVersion + " release",
+	commitId = "",
+cmd = {
+	/**
+	 * Run chain of commands and callbacks
+	 */
+	chain: function () {
+		var args = [].slice.call(arguments);
+		// @TODO run chain of commands with callbacks
+		async.eachSeries(args, function (item, callback) {
+			var command = "",
+				processFunction = null;
+
+			// if only string is in array then it is command without callback
+			if (typeof item === "string") {
+				command = item;
+			// if only function is in array then it is function to call, not operation
+			} else if (typeof item === "function") {
+				processFunction = item;
+			// if item is Array
+			} else if (item instanceof Array) {
+				// if first item is string then it is command
+				if (typeof item[0] === "string") {
+					command = item[0];
+				// if first item is function, then it is command template function
+				} else if (typeof item[0] === "function") {
+					command = item[0]();
 				}
-			);
-		},
-		/**
-		 * Run chain of commands and callbacks
-		 */
-		chain: function () {
-			// @TODO run chain of commands with callbacks
+
+				// if second item is function, then it is callback for command
+				if (typeof item[1] === "function") {
+					processFunction = item[1];
+				}
+			}
+
+			// if command exists then run it
+			if (command) {
+				console.log("Runnig: " + command);
+				exec(
+					command,
+					function (err, data, stderr) {
+						// when command return code different from 0 then display it and finish
+						if (err) {
+							console.error("exec error: " + err);
+							process.exit(err);
+						}
+						// if stderr is not empty then display it
+						if (stderr) {
+							console.error(stderr);
+						}
+						// if process callback is defined then process it with stdout
+						if (processFunction) {
+							processFunction(data, callback);
+						} else {
+							// otherwise got to next operation
+							callback();
+						}
+					}
+				);
+			} else {
+				// if process callback is defined without command then run it
+				if (processFunction) {
+					processFunction(callback);
+				} else {
+					// otherwise got to next operation
+					callback();
+				}
+			}
+		});
+	}
+};
+
+/**
+ * Async helper for read file, modify content and  write to the same file
+ * @param {string} fileName NAme of file
+ * @param {Function} operation Function with one string parameter and return string to save
+ * @param {Function} callback Callback inform that operation was end (async)
+ */
+function modifyFile(fileName, operation, callback) {
+	fs.readFile(fileName, function (err, data) {
+		var saveData = "";
+
+		// on errors display it and finish working
+		if (err) {
+			console.error(err);
+			process.exit(1);
 		}
-	};
-
-console.log("Running git describe --abbrev=0 --tags");
-cmd.get(
-	"git config user.email",
-	function (gitAccount) {
-		gitAccount = gitAccount.trim();
-		cmd.get(
-			"git config user.name",
-			function (userName) {
-				userName = userName.trim();
-				cmd.get(
-					"git describe --abbrev=0 --tags",
-					function (latestTag) {
-						var tauVersion = process.env.bamboo_deploy_release || process.env.bamboo_jira_version;
-
-						console.log("Running git log --pretty=oneline --no-merges " + latestTag.trim() + "..HEAD | sed -e 's/\[OAPTAU-[0-9]*\]//g' | sed -e 's/^\S* /- /g'")
-						cmd.get(
-							"git log --pretty=oneline --no-merges " + latestTag.trim() + "..HEAD | sed -e 's/ *\\\[OAPTAU-[0-9]*\\\]//g' | sed -e 's/^\\\S* /- /g'",
-							function (logLines) {
-								var now = moment().format("ddd MMM D YYYY");
-								logLines = "* " + now + " " + userName + " <" + gitAccount + "> " + tauVersion + "\n" + logLines.replace(/-/g, "\t-");
-								console.log(logLines);
-								fs.readFile(FILE_CHANGELOG, function (err, data) {
-									if (err) {
-										console.error(err);
-										process.exit(1);
-									}
-									logLines = data.toString().replace("%changelog", "%changelog\n" + logLines);
-									fs.writeFile(FILE_CHANGELOG, logLines, function (err, data) {
-										if (err) {
-											console.error(err);
-											process.exit(1);
-										}
-										console.log("Changelog was updated");
-										fs.readFile(FILE_SPEC, function (err, data) {
-											if (err) {
-												console.error(err);
-												process.exit(1);
-											}
-											data = data.toString().replace(/^Version:.*$/gm, "Version:    " + tauVersion);
-											console.log(data);
-											fs.writeFile(FILE_SPEC, data, function (err, data) {
-												if (err) {
-													console.error(err);
-													process.exit(1);
-												}
-												console.log("specfile was updated");
-												fs.readFile(FILE_PACKAGEJSON, function (err, package) {
-													if (err) {
-														console.error(err);
-														process.exit(1);
-													}
-													package = package.toString().replace(/"version":.*$/gm, "\"version\": \"" + tauVersion + "\",");
-													fs.writeFile(FILE_PACKAGEJSON, package, function (err, data) {
-														if (err) {
-															console.error(err);
-															process.exit(1);
-														}
-														console.log("package.json was updated");
-														cmd.get(
-															"git add " + FILE_CHANGELOG + " " + FILE_SPEC + " " + FILE_PACKAGEJSON,
-															function () {
-																var logMessage = "TAU " + tauVersion + " release";
-																console.log("Files added to git");
-																cmd.get(
-																	"git commit -s -m '" + logMessage + "'",
-																	function () {
-																		console.log("Files commited");
-																		cmd.get(
-																			"git push origin HEAD:refs/for/devel/tizen_3.0",
-																			function () {
-																				console.log("Commit pushed");
-																				cmd.get(
-																					"git log --format='%H' -n 1",
-																					function (commitId) {
-																						commitId = commitId.trim();
-																						cmd.get(
-																							"ssh -p 29418 165.213.149.170 gerrit set-reviewers -a t.lukawski@samsung.com " + commitId,
-																							function (data) {
-																								console.log("ssh -p 29418 165.213.149.170 gerrit set-reviewers -a t.lukawski@samsung.com " + commitId);
-																								console.log(data);
-																								console.log("Reviers added");
-																							});
-																					});
-																			});
-																	});
-															});
-													});
-												});
-											});
-										});
-									});
-								});
-							});
-					});
-			});
+		// prepare data to save
+		saveData = operation(data.toString());
+		// write to file
+		fs.writeFile(FILE_CHANGELOG, saveData, function (err) {
+			// on errors display it and finish working
+			if (err) {
+				console.error(err);
+				process.exit(1);
+			}
+			// go to next step
+			callback();
+		});
 	});
+}
+
+cmd.chain(
+	// check that version is set
+	function (callback) {
+		if (!tauVersion) {
+			console.error("Version is not set, use one of environment variables VERSION, bamboo_deploy_release, env.bamboo_jira_version");
+			process.exit(1);
+		}
+	},
+	// get user email form git
+	["git config user.email",
+		function (result, callback) {
+			gitAccount = result.trim();
+			callback();
+		}],
+	// get user name form git
+	["git config user.name",
+		function (result, callback) {
+			userName = result.trim();
+			callback();
+		}],
+	// get last tag
+	["git describe --abbrev=0 --tags",
+		function (result, callback) {
+			lastTag = result.trim();
+			callback();
+		}],
+	// get last changes
+	[
+		function () {
+			return "git log --pretty=oneline --no-merges " + lastTag + "..HEAD | sed -e 's/ *\\\[OAPTAU-[0-9]*\\\]//g' | sed -e 's/^\\\S* /- /g'"
+		},
+		function (logLines, callback) {
+			var now = moment().format("ddd MMM D YYYY");
+
+			logLines = "* " + now + " " + userName + " <" + gitAccount + "> " + tauVersion + "\n" + logLines.replace(/-/g, "\t-");
+			console.log(logLines);
+			// save changes to changelog
+			modifyFile(FILE_CHANGELOG, function (data) {
+				return data.replace("%changelog", "%changelog\n" + logLines);
+			}, callback);
+		}
+	],
+	// Version up in filespec
+	function (callback) {
+		modifyFile(FILE_SPEC, function (data) {
+			return data.replace(/^Version:.*$/gm, "Version:    " + tauVersion);
+		}, callback);
+	},
+	// Version up in package.json
+	function (callback) {
+		modifyFile(FILE_PACKAGEJSON, function (data) {
+			return data.replace(/^Version:.*$/gm, "Version:    " + tauVersion);
+		}, callback);
+	},
+	// add files to git
+	[function () {
+		return "git add " + FILE_CHANGELOG + " " + FILE_SPEC + " " + FILE_PACKAGEJSON;
+	}],
+	// create commit
+	[function () {
+		return "git commit -s -m '" + commitMessage + "'"
+	}],
+	// send to gerrit
+	"git push origin HEAD:refs/for/devel/tizen_3.0",
+	["git log --format='%H' -n 1",
+		function (result, callback) {
+			commitId = result.trim();
+			callback();
+		}],
+	// add reviewers
+	[function () {
+		return "ssh -p 29418 165.213.149.170 gerrit set-reviewers -a t.lukawski@samsung.com " + commitId;
+	}]
+);
