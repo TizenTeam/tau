69313a26421c88578e0d60a495371733fe1f33fd
Tomasz Lukawski <t.lukawski@samsung.com>
2017-02-17 15:46:57 +0100

    fix(wearable) Fix TCT for circular wearable config test (OAPTAU-223)
    - test for "config" for circular wearable had wrong test values
    
    Change-Id: I7be4b36c0aa42aab2aef2d165d17141d25b7eb2b
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/profile/wearable/config/config.js
+++ tau/tests/js/profile/wearable/config/config.js
@@ -1,13 +1,23 @@
-/* global document, tau, define, module, test, equal */
-
+/* global document, tau, define, module, test, equal*/
 function runTests(tau) {
 	module("profile/wearable/config");
 
-	test("config (wearable)", function () {
-		equal(tau.getConfig("enablePopupScroll"), false, "enablePopupScroll is set correct");
-		equal(tau.getConfig("popupTransition"), "slideup", "popupTransition is set correct");
-		equal(tau.getConfig("enablePageScroll"), false, "enablePageScroll is set correct");
-	});
+	if (tau.support.shape.circle) {
+		test("config (wearable)", function () {
+			equal(tau.getConfig("popupTransition"), "pop", "popupTransition is set correct");
+			equal(tau.getConfig("pageTransition"), "pop", "pageTransition is set correct");
+			equal(tau.getConfig("popupFullSize"), true, "popupFullSize is set correct");
+			equal(tau.getConfig("scrollEndEffectArea"), "screen", "scrollEndEffectArea is set correct");
+			equal(tau.getConfig("enablePageScroll"), true, "enablePageScroll is set correct");
+			equal(tau.getConfig("enablePopupScroll"), true, "enablePageScroll is set correct");
+		});
+	} else {
+		test("config (wearable)", function () {
+			equal(tau.getConfig("enablePopupScroll"), false, "enablePopupScroll is set correct");
+			equal(tau.getConfig("popupTransition"), "slideup", "popupTransition is set correct");
+			equal(tau.getConfig("enablePageScroll"), false, "enablePageScroll is set correct");
+		});
+	}
 }
 
 if (typeof define === "function") {
