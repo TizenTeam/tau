90812ea616136f6d8eb00fa5fc9eb9d3a9f25c29
Tomasz Lukawski <t.lukawski@samsung.com>
2017-03-07 15:49:31 +0100

    tests(mobile): fix TCT tests for js/jqm/router/router.html [OAPTAU-246]
    - test failed because of "pagechange" event has not triggered because of css file has not been loaded
    
    Change-Id: Ie7dbdaf387988e88cd5a85d4b12e60e08df32bf2
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/jqm/router/router.js
+++ tau/tests/js/jqm/router/router.js
@@ -1,29 +1,19 @@
 /*global module, $, asyncTest, document, ok, start, window, test */
 $().ready(function () {
 	"use strict";
-	var disabled = true;
 
 	module("jqm/router", {});
 
 	if (!window.navigator.userAgent.match("PhantomJS")) {
-		if (!disabled) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			asyncTest("$.mobile.changePage", 1, function () {
-				var page2 = $("#test2");
+		asyncTest("$.mobile.changePage", 1, function () {
+			var page2 = $("#test2");
 
-				$(document).on("pagechange", function () {
-					ok(page2.hasClass("ui-page-active"), "Check active page.");
-					start();
-				});
-				$.mobile.changePage(page2);
+			$(document).on("pagechange", function () {
+				ok(page2.hasClass("ui-page-active"), "Check active page.");
+				start();
 			});
-		} else {
-			test("Disabled test - TCT issue on device", function () {
-				ok(true, "Disabled test, TCT issue");
-			});
-		}
+			$.mobile.changePage(page2);
+		});
 	} else {
 		test("PhantomJS does not support XMLHttpRequest.responseType = document", function () {
 			ok(true, "bypassing");
