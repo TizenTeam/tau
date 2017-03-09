c856d05f7729e10419fd777dd5776d710a80ead7
Tomasz Lukawski <t.lukawski@samsung.com>
2017-03-09 13:52:55 +0100

    test(wearable): fix orientationchange tests for TCT [OAPTAU-248]
     - lack of default value of orientationchange.supported causes ambiguous action of test
    
    Change-Id: I54838769a1e3b3b40836bec7c95c2853f9ba71a8
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/core/event/orientationchange/orientationchange.js
+++ tau/tests/js/core/event/orientationchange/orientationchange.js
@@ -74,118 +74,104 @@
 			assert.equal(orientation.getOrientation(), "landscape", "landscape detected by orientation 180 degrees");
 		});
 
-		if (!window.tizen) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			QUnit.test("detection by matchMedia", function (assert) {
-				var queryListTrigger = null,
-					mockQueryList = {
-						matches: 0,
-						addListener: function (callback) {
-							queryListTrigger = callback;
-						}
-					};
-
-				mockWindow.matchMedia = function () {
-					return mockQueryList;
+		QUnit.test("detection by matchMedia", function (assert) {
+			var queryListTrigger = null,
+				mockQueryList = {
+					matches: 0,
+					addListener: function (callback) {
+						queryListTrigger = callback;
+					}
 				};
 
-				orientation.detect(); // default should be landscape since matches is 0
-				assert.equal(orientation.getOrientation(), "landscape", "landscape is default");
+			mockWindow.matchMedia = function () {
+				return mockQueryList;
+			};
 
-				mockQueryList.matches = 1;
-				orientation.detect();
-				assert.equal(orientation.getOrientation(), "portrait", "portrait detected");
+			orientation.supported = false;
 
-				mockQueryList.matches = 0;
-				queryListTrigger(mockQueryList, true);
-				assert.equal(orientation.getOrientation(), "landscape", "landscape when triggered");
+			orientation.detect(); // default should be landscape since matches is 0
+			assert.equal(orientation.getOrientation(), "landscape", "landscape is default");
 
-				mockQueryList.matches = 1;
-				queryListTrigger(mockQueryList, true);
-				assert.equal(orientation.getOrientation(), "portrait", "portrait when triggered");
+			mockQueryList.matches = 1;
+			orientation.detect();
+			assert.equal(orientation.getOrientation(), "portrait", "portrait detected");
+
+			mockQueryList.matches = 0;
+			queryListTrigger(mockQueryList, true);
+			assert.equal(orientation.getOrientation(), "landscape", "landscape when triggered");
+
+			mockQueryList.matches = 1;
+			queryListTrigger(mockQueryList, true);
+			assert.equal(orientation.getOrientation(), "portrait", "portrait when triggered");
+		});
+
+		QUnit.asyncTest("fire event for element", function (assert) {
+			var el = document.createElement("div");
+
+			el.addEventListener("orientationchange", function (e) {
+				assert.equal(e.detail.orientation, "portrait", "portrait is the default");
+				start();
 			});
-		}
 
-		if (!window.tizen) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			QUnit.asyncTest("fire event for element", function (assert) {
-				var el = document.createElement("div");
+			document.body.appendChild(el);
+			orientation.trigger(el);
+		});
 
-				el.addEventListener("orientationchange", function (e) {
-					assert.equal(e.detail.orientation, "portrait", "portrait is the default");
-					start();
-				});
-
-				document.body.appendChild(el);
-				orientation.trigger(el);
-			});
-		}
-
-		if (!window.tizen) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			QUnit.test("test orientationchange event recheck on mediaquery match", function (assert) {
-				var queryListTrigger = null,
-					mockQueryList = {
-						matches: 0,
-						addListener: function (callback) {
-							queryListTrigger = callback;
-						}
-					};
-
-				mockWindow.matchMedia = function () {
-					return mockQueryList;
+		QUnit.test("test orientationchange event recheck on mediaquery match", function (assert) {
+			var queryListTrigger = null,
+				mockQueryList = {
+					matches: 0,
+					addListener: function (callback) {
+						queryListTrigger = callback;
+					}
 				};
 
-				mockQueryList.matches = 1;
-				orientation.detect();
-				assert.equal(orientation.getOrientation(), "portrait", "portrait detected");
+			mockWindow.matchMedia = function () {
+				return mockQueryList;
+			};
 
-				mockQueryList.matches = 0; // set for landscape
-				mockWindow.innerWidth = 50;
-				mockWindow.innerHeight = 100; // force portrait by dimensions
+			orientation.supported = false;
 
-				// trigger callback without immiting orientationchange event triggering
-				// which will be catched by orientationchange handler, and passed to check by dimensions
-				// since orientation=null in mockWindow
-				queryListTrigger(mockQueryList);
-				assert.equal(orientation.getOrientation(), "portrait", "still set");
-			});
-		}
+			mockQueryList.matches = 1;
+			orientation.detect();
+			assert.equal(orientation.getOrientation(), "portrait", "portrait detected");
 
-		if (!window.tizen) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			QUnit.test("test unbind", function (assert) {
-				mockWindow.innerHeight = 50;
-				mockWindow.innerWidth = 100; // force landscape
+			mockQueryList.matches = 0; // set for landscape
+			mockWindow.innerWidth = 50;
+			mockWindow.innerHeight = 100; // force portrait by dimensions
 
-				orientation.detect();
+			// trigger callback without immiting orientationchange event triggering
+			// which will be catched by orientationchange handler, and passed to check by dimensions
+			// since orientation=null in mockWindow
+			queryListTrigger(mockQueryList);
+			assert.equal(orientation.getOrientation(), "portrait", "still set");
+		});
 
-				assert.equal(orientation.getOrientation(), "landscape", "proper orientation set");
+		QUnit.test("test unbind", function (assert) {
+			mockWindow.innerHeight = 50;
+			mockWindow.innerWidth = 100; // force landscape
 
-				mockWindow.innerHeight = 100;
-				mockWindow.innerWidth = 50; // force  portrait
+			orientation.supported = false;
 
-				helpers.triggerEvent(document.body, "throttledresize");
+			orientation.detect();
 
-				assert.equal(orientation.getOrientation(), "portrait", "proper orientation set by throttledresize");
+			assert.equal(orientation.getOrientation(), "landscape", "proper orientation set");
 
-				mockWindow.innerHeight = 50;
-				mockWindow.innerWidth = 100; // force landscape portrait
+			mockWindow.innerHeight = 100;
+			mockWindow.innerWidth = 50; // force  portrait
 
-				orientation.unbind();
-				helpers.triggerEvent(document.body, "throttledresize");
+			helpers.triggerEvent(document.body, "throttledresize");
 
-				assert.equal(orientation.getOrientation(), "portrait", "proper orientation still set by throttledresize");
-			});
-		}
+			assert.equal(orientation.getOrientation(), "portrait", "proper orientation set by throttledresize");
+
+			mockWindow.innerHeight = 50;
+			mockWindow.innerWidth = 100; // force landscape portrait
+
+			orientation.unbind();
+			helpers.triggerEvent(document.body, "throttledresize");
+
+			assert.equal(orientation.getOrientation(), "portrait", "proper orientation still set by throttledresize");
+		});
 	}
 
 	if (define) {
