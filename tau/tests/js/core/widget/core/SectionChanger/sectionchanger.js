3b76cf4f5505580f175f5f849e6f8cb42c25522a
Tomasz Lukawski <t.lukawski@samsung.com>
2017-03-20 12:48:27 +0100

    fix(wearable) Fix TCT tests for SectionChanger [OAPTAU-249]
    - wrong HTML template for test caused a issue in test - lack of "ui-page" markup
    
    Change-Id: Icb12f096de78fb27c2a0f5b96ac373a4b05b2d92
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tau/tests/js/core/widget/core/SectionChanger/sectionchanger.js
+++ tau/tests/js/core/widget/core/SectionChanger/sectionchanger.js
@@ -1,8 +1,8 @@
 /*global document, window, module, test, asyncTest, ok, setTimeout, start, equal, expect, strictEqual, helpers, QUnit,
-suites */
+ suites */
 /*
-* Unit Test: SectionChanger
-*/
+ * Unit Test: SectionChanger
+ */
 QUnit.config.reorder = false;
 
 (function (ns) {
@@ -194,62 +194,57 @@ QUnit.config.reorder = false;
 			}
 		});
 
-		if (!window.tizen) {
-			/**
-			 * @todo: disabled tests - TCT issues on device
-			 */
-			test("bouncing elements", 1, function test() {
-				equal(this.scrollbars.length, 2, "Scrollbar bouncing elements is 2");
-			});
+		test("bouncing elements", 1, function test() {
+			equal(this.scrollbars.length, 2, "Scrollbar bouncing elements is 2");
+		});
 
-			suites.bouncing.moves.forEach(function each(move, step) {
+		suites.bouncing.moves.forEach(function each(move, step) {
 
-				asyncTest(move.name, function swipe() {
-					var clientMoveXY = {
-							clientX: this.left + move.cord.x,
-							clientY: this.top + move.cord.y
-						},
-						sections = move.sections.before;
+			asyncTest(move.name, function swipe() {
+				var clientMoveXY = {
+						clientX: this.left + move.cord.x,
+						clientY: this.top + move.cord.y
+					},
+					sections = move.sections.before;
 
-					expect(6);
-					if (step === 1) {
-						expect(7);
-					}
+				expect(6);
+				if (step === 1) {
+					expect(7);
+				}
 
 
+				i = sections.length;
+
+				while (i--) {
+					equal(this.isActive(i), sections[i], "Section " + i + " before visible: " + sections[i]);
+				}
+
+				setTimeout(function setTimeout() {
+
+					// Simulate swiping
+					triggerTouchEvent(this.scroller, "touchstart", this.clientXY);
+					triggerTouchEvent(this.scroller, "touchmove", clientMoveXY);
+					triggerTouchEvent(this.scroller, "touchmove", clientMoveXY);
+					// End swiping
+					triggerTouchEvent(this.scroller, "touchend", clientMoveXY);
+
+					sections = move.sections.after;
 					i = sections.length;
-
 					while (i--) {
-						equal(this.isActive(i), sections[i], "Section " + i + " before visible: " + sections[i]);
+						equal(this.isActive(i), sections[i], "Section " + i + " after visible: " + sections[i]);
 					}
 
-					setTimeout(function setTimeout() {
+					if (step === 1) {
+						ok(this.scrollbars[0].classList.contains("ui-show"), "Left scrollbar bouncing is visibile");
+					}
 
-						// Simulate swiping
-						triggerTouchEvent(this.scroller, "touchstart", this.clientXY);
-						triggerTouchEvent(this.scroller, "touchmove", clientMoveXY);
-						triggerTouchEvent(this.scroller, "touchmove", clientMoveXY);
-						// End swiping
-						triggerTouchEvent(this.scroller, "touchend", clientMoveXY);
+					start();
 
-						sections = move.sections.after;
-						i = sections.length;
-						while (i--) {
-							equal(this.isActive(i), sections[i], "Section " + i + " after visible: " + sections[i]);
-						}
-
-						if (step === 1) {
-							ok(this.scrollbars[0].classList.contains("ui-show"), "Left scrollbar bouncing is visibile");
-						}
-
-						start();
-
-					}.bind(this), tapholdThreshold);
-
-				});
+				}.bind(this), tapholdThreshold);
 
 			});
-		}
+
+		});
 
 		suites.scrollbar.forEach(function each(suite) {
 
