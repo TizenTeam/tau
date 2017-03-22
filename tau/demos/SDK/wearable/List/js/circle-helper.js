647116ea5d6676831b885a09cdbaeb13bd7338f2
Przemyslaw Kepinski <p.kepinski@samsung.com>
2017-03-22 11:25:24 +0100

    fix(wearable): improving Template List structure [OAPTAU-308]
     - moved .js to to js/.js
     - adding newest circle helper
     - minor fixes in indeh.html
    
    Change-Id: Ib3286ec2a8cd2c2929765913879b0f9fe31b1ffe
    Signed-off-by: p.kepinski <p.kepinski@samsung.com>

--- tau/demos/SDK/wearable/List/js/circle-helper.js
+++ tau/demos/SDK/wearable/List/js/circle-helper.js
@@ -1,26 +1,43 @@
 /*global tau */
 /*jslint unparam: true */
 (function (tau) {
+	/**
+	 * page - Active page element
+	 * list - NodeList object for lists in the page
+	 * listHelper - Array for TAU snap list helper instances
+	 */
 	var page,
 		list,
 		listHelper = [],
 		i,
 		len;
 
+	// This logic works only on circular device.
 	if (tau.support.shape.circle) {
+		/**
+		 * pagebeforeshow event handler
+		 * Do preparatory works and adds event listeners
+		 */
 		document.addEventListener("pagebeforeshow", function (e) {
 			page = e.target;
-			list = page.querySelectorAll(".ui-listview");
-			if (list) {
-				len = list.length;
-				for (i = 0; i < len; i++) {
-					listHelper[i] = tau.helper.SnapListStyle.create(list[i], {animate: "scale"});
+			if (page.id !== "page-snaplistview" && page.id !== "page-swipelist") {
+				list = page.querySelector(".ui-listview");
+				if (list) {
+					listHelper.push(tau.widget.Listview(list));
 				}
 			}
 		});
 
+		/**
+		 * pagebeforehide event handler
+		 * Destroys and removes event listeners
+		 */
 		document.addEventListener("pagebeforehide", function () {
 			len = listHelper.length;
+			/**
+			 * Since the snap list helper attaches rotary event listener,
+			 * you must destroy the helper before the page is closed.
+			 */
 			if (len) {
 				for (i = 0; i < len; i++) {
 					listHelper[i].destroy();
