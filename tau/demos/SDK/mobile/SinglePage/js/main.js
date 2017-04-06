93dbe590de2962d822b3ecf4bf95a10c33d7ed47
p.milczarek2 <p.milczarek2@samsung.com>
2017-04-06 15:05:11 +0200

    fix(mobile): Improved Single Page template  [OAPTAU-297]
     - added global padding and changed the color of text
    
    Change-Id: Idc38af4bb35b55fc6aef04860b8044c6429d1fea
    Signed-off-by: p.milczarek2 <p.milczarek2@samsung.com>

--- tau/demos/SDK/mobile/SinglePage/js/main.js
+++ tau/demos/SDK/mobile/SinglePage/js/main.js
@@ -1,4 +1,7 @@
 (function () {
+	/**
+	 * Back key event handler
+	 */
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
 			page = null,
@@ -11,10 +14,16 @@
 
 			if (pageid === "main" && !activePopup) {
 				try {
+					/**
+					 * Exit application
+					 */
 					tizen.application.getCurrentApplication().exit();
 				} catch (ignore) {
 				}
 			} else {
+				/**
+				 * Go to previous browser window
+				 */
 				window.history.back();
 			}
 		}
