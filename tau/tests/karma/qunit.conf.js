e5909b8e990210a6a982da9984821202088db321
Maciej Urbanski <m.urbanski@samsung.com>
2017-04-13 13:45:40 +0200

    tool(Jenkins): Fix general problems in Jenkins [OAPTAU-359]
     - increase timeout for situation when conputer is overload
     - add screens based on emulator
     - update devel Jenkinsfile to production version
    
    Change-Id: I5c7cc6425e235d078dcb22e036122cde0ee7627b
    Signed-off-by: Maciej Urbanski <m.urbanski@samsung.com>

--- tau/tests/karma/qunit.conf.js
+++ tau/tests/karma/qunit.conf.js
@@ -88,6 +88,8 @@ module.exports = function (config) {
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: false
+		singleRun: false,
+		processKillTimeout: 10000,
+		browserDisconnectTimeout: 10000
 	});
 };
