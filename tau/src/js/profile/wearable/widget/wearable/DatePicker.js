6bf019f91fa07b30fea5e6d23afe7da826ec6bdc
d.dyrda <d.dyrda@samsung.com>
2017-08-10 14:46:25 +0200

    feature(wearable): Prepare static UI for DatePicker widget [OAPTAU-943]
    - create all required containers for each input field
    - updated stylesheet to match the UX guide
    
    Change-Id: I7715e266afd2cfc7297835e240b31178d5508a31
    Signed-off-by: d.dyrda <d.dyrda@samsung.com>

--- tau/src/js/profile/wearable/widget/wearable/DatePicker.js
+++ tau/src/js/profile/wearable/widget/wearable/DatePicker.js
@@ -8,15 +8,23 @@
 		[
 			"../../../../core/engine",
 			"../../../../core/widget/BaseWidget",
+			"./NumberPicker",
 			"../wearable"
 		],
 
 		function () {
 			//>>excludeEnd("tauBuildExclude");
 			var BaseWidget = ns.widget.BaseWidget,
+				NumberPicker = ns.widget.wearable.NumberPicker,
 				prototype = new BaseWidget(),
 
 				WIDGET_CLASS = "ui-date-picker",
+				classes = {
+					MONTH_CONTAINER: WIDGET_CLASS + "-container-month",
+					DAY_CONTAINER: WIDGET_CLASS + "-container-day",
+					YEAR_CONTAINER: WIDGET_CLASS + "-container-year",
+					DAYNAME_CONTAINER: WIDGET_CLASS + "-containter-dayname"
+				},
 
 				WIDGET_SELECTOR = "." + WIDGET_CLASS;
 
@@ -24,7 +32,78 @@
 			}
 
 			prototype._build = function (element) {
+				var self = this,
+					footer = document.createElement("footer"),
+					buttonSet = document.createElement("button"),
+					monthContainer = self._createContainter("month"),
+					dayContainer = self._createContainter("day"),
+					yearContainer = self._createContainter("year"),
+					dayNameContainer = document.createElement("div");
+
+				// create button set
+				buttonSet.innerHTML = "SET";
+				// add classes
+				element.classList.add(NumberPicker.classes.CONTAINER);
+				buttonSet.classList.add("ui-btn", NumberPicker.classes.BUTTON_SET);
+				footer.classList.add("ui-footer", "ui-bottom-button", "ui-fixed");
+
+				monthContainer.classList.add(classes.MONTH_CONTAINER);
+				dayContainer.classList.add(classes.DAY_CONTAINER);
+				yearContainer.classList.add(classes.YEAR_CONTAINER);
+				dayNameContainer.classList.add(classes.DAYNAME_CONTAINER);
+
+				dayNameContainer.innerHTML = "Wednesday";
+
+				// build DOM structure
+				element.appendChild(monthContainer);
+				element.appendChild(dayContainer);
+				element.appendChild(yearContainer);
+				element.appendChild(dayNameContainer);
+				element.appendChild(footer);
+
+				footer.appendChild(buttonSet);
+
 				return element;
+			};
+
+			prototype._createContainter = function (name) {
+				var number = document.createElement("span"),
+					numberPickerInput = document.createElement("input"),
+					numberPickerLabel = document.createElement("label"),
+					numberPickerContainer = document.createElement("div");
+
+				number.classList.add(NumberPicker.classes.NUMBER);
+				numberPickerLabel.classList.add(NumberPicker.classes.LABEL);
+
+				switch (name) {
+					case "month":
+						numberPickerLabel.innerText = "Month";
+						numberPickerInput.max = 12;
+						number.innerHTML = "Dec";
+						break;
+					case "day":
+						numberPickerLabel.innerText = "Day";
+						numberPickerInput.max = 31;
+						number.innerHTML = "20";
+						break;
+					case "year":
+						numberPickerLabel.innerText = "Year";
+						numberPickerInput.max = 2999;
+						number.innerHTML = "2015";
+						break;
+				}
+
+				numberPickerInput.min = "1";
+				numberPickerInput.step = "1";
+				numberPickerContainer.appendChild(numberPickerLabel);
+				numberPickerContainer.appendChild(number);
+				numberPickerContainer.appendChild(numberPickerInput);
+
+				return numberPickerContainer;
+			};
+
+			prototype._daysInMonth = function (year, month) {
+				return new Date(year, month - 1, 0);
 			};
 
 			DatePicker.prototype = prototype;
